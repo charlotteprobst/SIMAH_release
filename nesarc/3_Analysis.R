d4676ad67eaab92c4b437bf47db3c6ace147503b
Klajdi Puka <klajdi.puka@gmail.com>
2021-09-28 09:07:39 -0400

    Update 3_Analysis.R

--- nesarc/3_Analysis.R
+++ nesarc/3_Analysis.R
@@ -89,7 +89,6 @@ alc5.msm <- readRDS(paste0(output, "alc5.msm.RDS"))
       hazard.msm(alc5.msm)
 
 
-
 # Table 2 - Extract Annual Transition Probabilities (aTP) and correct CI to original sample size 
 aTP.results <- data.frame(print(pmatrix.msm(alc5.msm, t=1, ci="norm"))) %>%
   mutate(From = row.names(.)) %>%
@@ -141,9 +140,9 @@ HR.results <- data.frame(hazard.msm(alc5.msm))%>%
   
   
   
-# Extract and Plot TP --------------------------------------------------------------------------------------------------
+# Extract and Plot TP at each level of each covariate --------------------------------------------------------------------------------------------------
 
-# Function to extract TP over over multiple years
+# Function to extract TP 
 extract_TP <- function(model, age_z, sex, race, edu) {
   probs <- list()
   for (i in age_z){
@@ -161,19 +160,22 @@ extract_TP <- function(model, age_z, sex, race, edu) {
             rename(Probability = value) %>%
             mutate(
               # Recode values
-              From = recode(From, "State 1" = "Lifetime abstainer", 
-                "State 2" = "Former drinker",
-                "State 3" = "Low risk",
-                "State 4" = "Medium risk",
-                "State 5" = "High risk"), 
-              To = recode(To, "State.1" = "Lifetime abstainer", 
-                "State.2" = "Former drinker",
-                "State.3" = "Low risk",
-                "State.4" = "Medium risk",
-                "State.5" = "High risk"),
-              Transition = paste(From, To, sep = " -> "),
+              From = recode(From, "State 1" = "Abstainer", 
+                "State 2" = "Former",
+                "State 3" = "Category I",
+                "State 4" = "Category II",
+                "State 5" = "Category III"), 
+              To = recode(To, "State.1" = "Abstainer", 
+                "State.2" = "Former",
+                "State.3" = "Category I",
+                "State.4" = "Category II",
+                "State.5" = "Category III"),
+              Transition = paste(From, To, sep = "->"),
+              Transition = fct_relevel(Transition, "Abstainer->Category I",     # re-arrange order of transition variable
+                "Former->Category I",	"Category I->Category II",	"Category II->Category III",
+                "Category III->Category II",	"Category II->Category I",	"Category I->Former"),
               age_z = i,
-              age = (age_z * 16.9995) + 46.28592,   # SD of original age variable: 16.9995; mean: 46.28592 
+              age = round((age_z * 16.9995) + 46.28592, digits = 0),   # SD of original age variable: 16.9995; mean: 46.28592 
               sex = j, 
               race = l,
               edu = k) 
@@ -187,6 +189,92 @@ extract_TP <- function(model, age_z, sex, race, edu) {
   return(probs)
   
 }
+extract_TP_atmean_race <- function(model, age_z, sex, edu) {
+    probs <- list()
+    for (i in age_z){
+      for (j in sex){
+        for (k in edu){
+            # extract the probabilities
+            probs[[paste(i,j,k)]] <- data.frame(print(pmatrix.msm(model, t=1, 
+              covariates = list(age_scaled = i,   female_wave1.factor = j, edu3.factor = k)))) %>%
+              
+              # modify the output presentation
+              mutate(From = row.names(.)) %>%
+              pivot_longer(cols = -From, names_to = "To") %>%
+              rename(Probability = value) %>%
+              mutate(
+                # Recode values
+                From = recode(From, "State 1" = "Abstainer", 
+                  "State 2" = "Former",
+                  "State 3" = "Category I",
+                  "State 4" = "Category II",
+                  "State 5" = "Category III"), 
+                To = recode(To, "State.1" = "Abstainer", 
+                  "State.2" = "Former",
+                  "State.3" = "Category I",
+                  "State.4" = "Category II",
+                  "State.5" = "Category III"),
+                Transition = paste(From, To, sep = "->"),
+                Transition = fct_relevel(Transition, "Abstainer->Category I",     # re-arrange order of transition variable
+                  "Former->Category I",	"Category I->Category II",	"Category II->Category III",
+                  "Category III->Category II",	"Category II->Category I",	"Category I->Former"),
+                age_z = i,
+                age = round((age_z * 16.9995) + 46.28592, digits = 0),   # SD of original age variable: 16.9995; mean: 46.28592 
+                sex = j, 
+                edu = k) 
+        }
+      }
+    }
+    
+    probs <- do.call(rbind,probs)
+    row.names(probs) <- NULL  # remove row names
+    return(probs)
+    
+}
+extract_TP_atmean_edu <- function(model, age_z, sex, race) {
+  probs <- list()
+  for (i in age_z){
+    for (j in sex){
+      for (l in race){
+          # extract the probabilities
+          probs[[paste(i,j,l)]] <- data.frame(print(pmatrix.msm(model, t=1, 
+            covariates = list(age_scaled = i,   female_wave1.factor = j, race_wave1.factor = l)))) %>%
+            
+            # modify the output presentation
+            mutate(From = row.names(.)) %>%
+            pivot_longer(cols = -From, names_to = "To") %>%
+            rename(Probability = value) %>%
+            mutate(
+              # Recode values
+              From = recode(From, "State 1" = "Abstainer", 
+                "State 2" = "Former",
+                "State 3" = "Category I",
+                "State 4" = "Category II",
+                "State 5" = "Category III"), 
+              To = recode(To, "State.1" = "Abstainer", 
+                "State.2" = "Former",
+                "State.3" = "Category I",
+                "State.4" = "Category II",
+                "State.5" = "Category III"),
+              Transition = paste(From, To, sep = "->"),
+              Transition = fct_relevel(Transition, "Abstainer->Category I",     # re-arrange order of transition variable
+                "Former->Category I",	"Category I->Category II",	"Category II->Category III",
+                "Category III->Category II",	"Category II->Category I",	"Category I->Former"),
+              age_z = i,
+              age = round((age_z * 16.9995) + 46.28592, digits = 0),   # SD of original age variable: 16.9995; mean: 46.28592 
+              sex = j, 
+              race = l) 
+      }
+    }
+  }
+  
+  probs <- do.call(rbind,probs)
+  row.names(probs) <- NULL  # remove row names
+  return(probs)
+  
+}
+
+  
 
 # First, specify the covariate values
 age_z <- sort(unique(nesarc_expanded$age_scaled))
@@ -196,35 +284,101 @@ edu <- unique(nesarc_expanded$edu3.factor)
 
 # Run function to extract TP
 aTP <- extract_TP(alc5.msm, age_z, sex, race, edu)
-  
+aTP_atmean_race <- extract_TP_atmean_race(alc5.msm, age_z, sex, edu) # TP at mean value of race (for plots)
+aTP_atmean_edu <- extract_TP_atmean_edu(alc5.msm, age_z, sex, race) # TP at mean value of edu (for plots)
+
+
 # Save TP
 write_csv(aTP, paste0(output, "Transition Probabilities.csv"))
 
-# Load TP
-aTP <- read_csv(paste0(output, "Transition Probabilities.csv"))
 
 
-# Plot Transition probabilities *******************************************************************
+### Plot Transition probabilities --------------------------------------------------------------------------
 # 'Poor' Transitions, among non-Hispanic Whites 
 aTP %>%
-  filter(Transition %in%c("Lifetime abstainer -> Low risk", "Former drinker -> Low risk",  "Low risk -> Medium risk", "Medium risk -> High risk")) %>%
+  filter(Transition %in% c("Abstainer->Category I", "Former->Category I",  "Category I->Category II", "Category II->Category III")) %>%
   filter(race=="White, non-Hispanic") %>%
   ggplot(aes(x=age, y=Probability, color=edu, linetype=edu)) + geom_line(size=1)+
   facet_grid(cols=vars(sex), rows=vars(Transition), scale="free") +
-  scale_x_continuous(limits=c(18, 85), breaks=seq(20, 85, by= 5)) + 
-  labs(title= "'Poor' Transitions", subtitle="White, non-Hispanic", x = "age", y="Transition probability") + 
-  theme_bw() + theme(legend.position="bottom")
+  scale_x_continuous(limits=c(18, 90), breaks=seq(20, 90, by= 5)) + 
+  labs(title= "'Poor' Transitions", subtitle="White, non-Hispanic", 
+        color="Education:", linetype="Education:",
+        x = "Age (years)", y="Transition probability") + 
+  theme_bw() + theme(legend.position=c(0.8, 1.1), legend.direction="horizontal")
+ggsave(paste0(output, "Figure 1a.tiff"), dpi=600, width=7.5)
+
 
 
 # 'Better' Transitions, among non-Hispanic Whites 
 aTP %>%
-  filter(Transition %in%c("High risk -> Medium risk", "Medium risk -> Low risk", "Low risk -> Former drinker")) %>%
+  filter(Transition %in% c("Category III->Category II", "Category II->Category I", "Category I->Former")) %>%
   filter(race=="White, non-Hispanic") %>%
-  ggplot(aes(x=as.numeric(age), y=Probability, color=edu, linetype=edu)) + geom_line(size=1.5)+
+  ggplot(aes(x=age, y=Probability, color=edu, linetype=edu)) + geom_line(size=1)+
   facet_grid(cols=vars(sex), rows=vars(Transition), scale="free") +
-  # scale_x_continuous(limits=c(18, 85), breaks=seq(20, 85, by= 5)) + 
-  labs(title= "'Better' Transitions", subtitle="White, non-Hispanic", x = "age", y="Transition probability") + 
-  theme_bw() + theme(legend.position="bottom")
+  scale_x_continuous(limits=c(18, 90), breaks=seq(20, 90, by= 5)) + 
+  labs(title= "'Poor' Transitions", subtitle="White, non-Hispanic", 
+    color="Education:", linetype="Education:",
+    x = "Age (years)", y="Transition probability") + 
+  theme_bw() + theme(legend.position=c(0.8, 1.1), legend.direction="horizontal")
+ggsave(paste0(output, "Figure 1b.tiff"), dpi=600, width=7.5)
+
+
+### Plot Transition probabilities at mean race --------------------------------------------------------------------------
+
+# 'Poor' Transitions
+aTP_atmean_race %>%
+  filter(Transition %in% c("Abstainer->Category I", "Former->Category I",  "Category I->Category II", "Category II->Category III")) %>%
+  ggplot(aes(x=age, y=Probability, color=edu, linetype=edu)) + geom_line(size=1)+
+  facet_grid(cols=vars(sex), rows=vars(Transition), scale="free") +
+  scale_x_continuous(limits=c(18, 90), breaks=seq(20, 90, by= 5)) + 
+  labs(title= "'Poor' Transitions", 
+    color="Education:", linetype="Education:",
+    x = "Age (years)", y="Transition probability") + 
+  theme_bw() + theme(legend.position=c(0.8, 1.075), legend.direction="horizontal")
+ggsave(paste0(output, "TP by edu 1.tiff"), dpi=600, width=7.5)
+
+
+# 'Better' Transitions 
+aTP_atmean_race %>%
+  filter(Transition %in% c("Category III->Category II", "Category II->Category I", "Category I->Former")) %>%
+  ggplot(aes(x=age, y=Probability, color=edu, linetype=edu)) + geom_line(size=1)+
+  facet_grid(cols=vars(sex), rows=vars(Transition), scale="free") +
+  scale_x_continuous(limits=c(18, 90), breaks=seq(20, 90, by= 5)) + 
+  labs(title= "'Better' Transitions", 
+    color="Education:", linetype="Education:",
+    x = "Age (years)", y="Transition probability") + 
+  theme_bw() + theme(legend.position=c(0.8, 1.075), legend.direction="horizontal")
+ggsave(paste0(output, "TP by edu 2.tiff"), dpi=600, width=7.5)
+
+
+
+### Plot Transition probabilities at mean edu --------------------------------------------------------------------------
+# 'Poor' Transitions
+aTP_atmean_edu %>%
+  filter(Transition %in% c("Abstainer->Category I", "Former->Category I",  "Category I->Category II", "Category II->Category III")) %>%
+  ggplot(aes(x=age, y=Probability, color=race, linetype=race)) + geom_line(size=1)+
+  facet_grid(cols=vars(sex), rows=vars(Transition), scale="free") +
+  scale_x_continuous(limits=c(18, 90), breaks=seq(20, 90, by= 5)) + 
+  labs(title= "'Better' Transitions", subtitle="",
+    color="Race/Ethnicity:", linetype="Race/Ethnicity:",
+    x = "Age (years)", y="Transition probability") + 
+  theme_bw() + theme(legend.position=c(0.7, 1.1), legend.direction="horizontal") + guides(color = guide_legend(nrow = 2))
+ggsave(paste0(output, "TP by race 1.tiff"), dpi=600, width=7.5)
+
+
+
+# 'Better' Transitions 
+aTP_atmean_edu %>%
+  filter(Transition %in% c("Category III->Category II", "Category II->Category I", "Category I->Former")) %>%
+  ggplot(aes(x=age, y=Probability, color=race, linetype=race)) + geom_line(size=1)+
+  facet_grid(cols=vars(sex), rows=vars(Transition), scale="free") +
+  scale_x_continuous(limits=c(18, 90), breaks=seq(20, 90, by= 5)) + 
+  labs(title= "'Better' Transitions", subtitle="",
+    color="Race/Ethnicity:", linetype="Race/Ethnicity:",
+    x = "Age (years)", y="Transition probability") + 
+  theme_bw() + theme(legend.position=c(0.7, 1.1), legend.direction="horizontal") + guides(color = guide_legend(nrow = 2))
+ggsave(paste0(output, "TP by race 2.tiff"), dpi=600, width=7.5)
+
 
 
 # TP over over multiple years ---------------------------------------------------------------------------------------------
@@ -250,16 +404,20 @@ extract_TP_over_yrs <- function(model, max_years) {
         Estimate = round(Estimate*100, digits=1),
         
         # Recode values
-        From = recode(From, "State 1" = "Lifetime abstainer", 
-                            "State 2" = "Former drinker",
-                            "State 3" = "Low risk",
-                            "State 4" = "Medium risk",
-                            "State 5" = "High risk"), 
-        To = recode(To, "State.1" = "Lifetime abstainer", 
-                        "State.2" = "Former drinker",
-                        "State.3" = "Low risk",
-                        "State.4" = "Medium risk",
-                        "State.5" = "High risk"), 
+        From = recode(From, "State 1" = "Initial State: Abstainer", 
+                            "State 2" = "Initial State: Former",
+                            "State 3" = "Initial State: Category I",
+                            "State 4" = "Initial State: Category II",
+                            "State 5" = "Initial State: Category III"), 
+        To = recode(To, "State.1" = "Abstainer", 
+                        "State.2" = "Former",
+                        "State.3" = "Category I",
+                        "State.4" = "Category II",
+                        "State.5" = "Category III"), 
+        # Re-arrange order
+        From = fct_relevel(From, "Initial State: Abstainer",  "Initial State: Former",	"Initial State: Category I",	"Initial State: Category II", "Initial State: Category III"),
+        To = fct_relevel(To, "Abstainer",  "Former",	"Category I",	"Category II", "Category III"),
+        
         Year = i) %>%
       select (From, To, Year, Estimate, newLower, newUpper) %>%
       na.omit() # remove NAs - transitions back to 'Abstainer'
@@ -271,17 +429,16 @@ extract_TP_over_yrs <- function(model, max_years) {
 
 # Extract TP over multiple years and save reults 
 TP_years <- extract_TP_over_yrs(alc5.msm, 10)
-write_csv(TP_years, paste0(output, "TP over time.csv"))
 
 # Load TP over multiple years and plot
-TP_years <- read_csv(paste0(output, "TP over time.csv"))
 TP_years %>%
-  ggplot(aes(x=Year, y=Estimate, ymin=newLower, ymax=newUpper, color=To, fill=To)) + 
-  geom_line(size=1) + geom_ribbon(alpha=0.1) +
+  ggplot(aes(x=Year, y=Estimate, group=To)) + geom_line(aes(color=To), size=0.5) + 
+  geom_ribbon(aes(ymin=newLower, ymax=newUpper, fill=To), alpha=0.2) + 
   facet_wrap(~From) +
   theme_bw() + labs(x = "Years Follow-up", y="Transition probability", color="Transition To", fill="Transition To") +
-  theme(legend.position = c(.85, 0.25))
-
-
+  theme(legend.position = c(.85, 0.25)) +
+  scale_y_continuous(breaks=seq(0, 100, by= 10)) + 
+  scale_x_continuous(breaks=seq(0, 10, by= 2))
+ggsave(paste0(output, "TP over time.tiff"), dpi=600, width=7.5)
 
 
