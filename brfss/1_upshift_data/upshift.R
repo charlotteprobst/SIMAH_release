7317a0c9a0d95fc992d8d1753de08e1bb9b869a1
charlottembuckley <c.m.buckley@sheffield.ac.uk>
2021-10-22 17:46:44 +0100

    update of upshifting code for BRFSS state level data

--- brfss/1_upshift_data/upshift.R
+++ brfss/1_upshift_data/upshift.R
@@ -8,17 +8,124 @@ library(dplyr)
 library(tidyr)
 library(labelled)
 library(sjlabelled)
+library(tidyverse)
 
 # CB laptop directory
 wd <- "~/Google Drive/SIMAH Sheffield/"
 # CB desktop directory
-wd <- "G:/My Drive/SIMAH Sheffield/"
+# wd <- "G:/My Drive/SIMAH Sheffield/"
 setwd(wd)
 
 ####read in the joined up data files 
 dataFiles <- readRDS("SIMAH_workplace/brfss/processed_data/brfss_full_selected.RDS")
 gc()
 
+source("SIMAH_code/brfss/1_upshift_data/upshift_functions.R")
+
+SIMAH_states <- c("California","Colorado","Florida","Indiana","Kentucky",
+                  "Louisiana","Massachustts","Michigan","Minnesota","Missouri",
+                  "New York","Oregon","Pennsylvania","Tennessee","Texas")
+# first subset for selected SIMAH states 
+dataFiles <- lapply(dataFiles, subset_SIMAH_states)
+
+# now remove any missing data for key variables 
+dataFiles <- lapply(dataFiles, remove_missing)
+
 # first exploring the data for our selected states and for the US current prevalence and quantity (monthly)
 summary <- lapply(dataFiles, summariseprevalence)
-summary <- do.call(rbind,summary)
+summary <- do.call(rbind,summary) %>% filter(drinkingstatus==1) %>% 
+  drop_na()
+ggplot(data=summary, aes(x=YEAR, y=percentage, colour=sex_recode)) + 
+  geom_line() + facet_wrap(~agecat+State) + theme_bw()
+# reassign the list such that each state is one element of the list 
+data <- do.call(rbind, dataFiles)
+
+test <- data %>% filter(drinkingstatus==1 & gramsperday==0)
+# some people claim to be drinkers but quantity per occasion =0 
+# solution (for now) is to allocate small amount of drinking per occasion 
+data$quantity_per_occasion <- ifelse(data$drinkingstatus==1 & data$gramsperday==0,
+                                     0.01, data$quantity_per_occasion)
+data$gramsperday <- data$quantity_per_occasion*data$alc_frequency/30*14
+summary(data$gramsperday)
+
+# put cap of 200gpd
+data$gramsperday <- ifelse(data$gramsperday>200, 200, data$gramsperday)
+
+dataFiles <- list()
+
+for(i in SIMAH_states){
+  dataFiles[[paste(i)]] <- data %>% filter(State==i)
+}
+
+# allocate individuals to be monthly/yearly/former drinkers or lifetime abstainers
+data <- impute_yearly_drinking(data)
+
+# check imputation has worked
+summary <- data %>% group_by(YEAR, State, drinkingstatus_detailed) %>% 
+  tally() %>% ungroup() %>% 
+  group_by(YEAR, State) %>% 
+  mutate(percent = n/sum(n))
+ggplot(data=summary, aes(x=YEAR, y=percent, fill=drinkingstatus_detailed)) + 
+  geom_bar(stat="identity",position="stack") + 
+  facet_wrap(~State)
+# year 2000 data is not available for California, Colorado, Florida,
+# Indiana, Kentucky, Louisiana, Michigan, Missouri, New York, 
+# Oregon, Pennsylvania - use 1999 data for 2000 instead
+
+# impute the gpd for the non-30 day drinkers from the NAS data
+NASGPD <- read.csv("SIMAH_workplace/brfss/processed_data/NAS_GPD_non30day.csv") %>% 
+  rename(sex_recode = SEX, 
+         agecat = AGECAT, 
+         race_eth=RACE) %>% dplyr::select(-imputeddrinking) %>% 
+  mutate(drinkingstatus_detailed = "Yearly drinker")
+
+data <- left_join(data, NASGPD) %>% 
+  mutate(gramsperday = ifelse(drinkingstatus_detailed=="Yearly drinker",
+                              ALCGPD_non30, gramsperday)) %>% 
+  dplyr::select(-ALCGPD_non30)
+
+# read in APC data - source = NIAAA 
+# https://pubs.niaaa.nih.gov/publications/surveillance117/pcyr1970-2019.txt
+APC <- process_APC(data)
+
+# now join this up with the data
+data <- left_join(data, APC)
+
+tally <- data %>% group_by(YEAR, State, drinkingstatus_updated) %>% 
+  tally() %>% ungroup() %>% 
+  group_by(YEAR, State) %>% 
+  mutate(percentdrinkers = n/sum(n)) %>% dplyr::select(-n)
+
+# now calculate the percentage of drinkers in each year in each state
+data <- left_join(data,tally)
+
+# perform the up-shift 
+data <- data %>% group_by(YEAR, State) %>% 
+  mutate(BRFSS_APC = mean(gramsperday),
+         adj_brfss_apc = BRFSS_APC/percentdrinkers,
+         quotient = (gramsperday_adj*0.9)/adj_brfss_apc,
+         cr_quotient = quotient^(1/3),
+         gramsperday_upshifted = gramsperday*(quotient))
+
+# now compare up-shifted to per capita mean data for each state 
+compare <- data %>% 
+  dplyr::select(YEAR, State, gramsperday_adj, adj_brfss_apc, BRFSS_APC, gramsperday_upshifted) %>% 
+  group_by(YEAR, State) %>% 
+  summarise(SALES=mean(gramsperday_adj), BASELINE=mean(adj_brfss_apc))
+
+percapita_adjusted <- data %>% 
+  filter(drinkingstatus_updated==1) %>% 
+  group_by(YEAR,State) %>% 
+  summarise(ADJUSTED=mean(gramsperday_upshifted, na.rm=T))
+
+compare <- left_join(compare, percapita_adjusted) %>% 
+  pivot_longer(cols=SALES:ADJUSTED)
+
+ggplot(data=compare, aes(x=YEAR, y=value, colour=name)) + geom_line() +
+  facet_wrap(~State) + theme_bw() + theme(legend.title=element_blank(),
+                                          legend.position="bottom") + 
+  ylim(0,NA)
+ggsave("SIMAH_Workplace/brfss/upshifted_plots.png", dpi=300, width=33, height=19, units="cm")
+
+
+
--- brfss/1_upshift_data/upshift_functions.R
+++ brfss/1_upshift_data/upshift_functions.R
@@ -1,7 +1,122 @@
-summariseprevalence <- function(data){
-  data <- data %>% group_by(YEAR, State, sex_recode,drinkingstatus) %>% 
-    summarise(total = sum(final_sample_weight)) %>% 
-    ungroup() %>% 
-    group_by(YEAR, State, sex_recode) %>% 
-    mutate(percentage = total / sum(total))
+subset_SIMAH_states <- function(data){
+# subset for SIMAH states 
+SIMAH_states <- c("California","Colorado","Florida","Indiana","Kentucky",
+                  "Louisiana","Massachustts","Michigan","Minnesota","Missouri",
+                  "New York","Oregon","Pennsylvania","Tennessee","Texas")
+data <- data %>% ungroup() %>% filter(State %in% SIMAH_states)
+return(data)
 }
+
+remove_missing <- function(data){
+  data <- data %>% dplyr::select(YEAR, State, race_eth,
+                                 sex_recode, age_var,
+                                 employment, education_summary,
+                                 household_income,
+                                 BMI, drinkingstatus,
+                                 alc_frequency, quantity_per_occasion,
+                                 gramsperday,
+                                 hed) %>% drop_na()
+  return(data)
+}
+
+summariseprevalence <- function(data){
+  data <- data %>% 
+    mutate(agecat = cut(age_var,
+                        breaks=c(0,34,64,100),
+                        labels=c("18-34","35-64","65+"))) %>% 
+    group_by(YEAR, State, sex_recode,agecat,drinkingstatus) %>% 
+    tally(name="total") %>% 
+    # summarise(total = sum(final_sample_weight)) %>% 
+    ungroup() %>% 
+    group_by(YEAR, State, sex_recode,agecat) %>% 
+    mutate(percentage = total / sum(total))
+  return(data)
+  }
+
+process_APC <- function(data){
+  APC <- read.delim("SIMAH_workplace/brfss/processed_data/pcyr1970-2019.txt", skip=131, header=F, sep="")
+  names(APC)[1] <- "Year"
+  names(APC)[2] <- "State"
+  names(APC)[3] <- "BeverageType"
+  names(APC)[7] <- "Gallons per capita"
+  APC <- APC %>% filter(BeverageType==4) %>% dplyr::select(Year, State, 'Gallons per capita') %>% 
+    mutate(`Gallons per capita` = `Gallons per capita`/10000)
+  USA <- APC %>% group_by(Year) %>% summarise(`Gallons per capita`= mean(`Gallons per capita`)) %>% 
+    mutate(State="USA")
+  APC <- rbind(APC,USA)
+  APC <- APC %>% 
+    mutate(State = recode(State,
+                          "6"="California", "8"="Colorado", "12"="Florida",
+                          "18"="Indiana", "21"="Kentucky", "22"="Louisiana",
+                          "25"="Massachusetts", "26"="Michigan", "27"="Minnesota",
+                          "29"="Missouri","36"="New York","41"="Oregon",
+                          "42"="Pennsylvania","47"="Tennessee","48"="Texas",
+                          "USA"="USA", .default="NA"),
+           State = ifelse(State=="NA",NA,State)) %>% 
+    drop_na() %>% filter(Year>=min(data$YEAR)) %>% 
+    rename(Gallons = 'Gallons per capita') %>% 
+    mutate(litrespercapita = Gallons*3.78541)
+    
+  # allocate 2020 data 2019 APC UNTIL 2020 IS RELEASED
+  data2020 <- expand.grid(Year=2020, State=unique(data$State)) %>% 
+    mutate(State=as.character(State))
+  data2020$Gallons <- NA
+  data2020$litrespercapita=NA
+  APC <- rbind(APC, data2020)
+  APC <- APC %>% group_by(State) %>% 
+    fill(Gallons,litrespercapita) %>% mutate(Gallons=as.numeric(Gallons),
+                                             litrespercapita=as.numeric(litrespercapita)) %>% 
+    mutate(gramsperday = (litrespercapita*785.06)/365,
+           gramsperday_adj = gramsperday - (gramsperday*0.0098)) %>% 
+    dplyr::select(Year, State, gramsperday_adj) %>% 
+    rename(YEAR=Year)
+  return(APC)
+}
+
+impute_yearly_drinking <- function(data){
+  #####STEP 1 
+  ####TURNING THE NON 30-DAY DRINKERS INTO YEARLY DRINKERS, FORMER DRINKERS, ABSTAINERS
+  
+  ####read in the NAS distributions
+  NASdistributions <- read.csv("SIMAH_workplace/brfss/processed_data/NAS_distributions.csv") %>% 
+    mutate(Age = gsub("Age ", "", Age),
+           YEARLYDRINKERS = Yearly...non.30d..Drinkers / (Yearly...non.30d..Drinkers+Former..Drinker+Abstainer),
+           FORMERDRINKERS = Former..Drinker / (Yearly...non.30d..Drinkers+Former..Drinker+Abstainer),
+           ABSTAINERS = Abstainer / (Yearly...non.30d..Drinkers+Former..Drinker+Abstainer),
+           group=paste(Sex, Age, Race, sep="_")) %>% 
+    dplyr::select(group, YEARLYDRINKERS, FORMERDRINKERS, ABSTAINERS) %>% 
+    pivot_longer(cols=YEARLYDRINKERS:ABSTAINERS) %>% 
+    group_by(group) %>% 
+    mutate(cumsum = cumsum(value))
+  
+  data <- data %>% mutate(agecat = cut(age_var, 
+                                       breaks=c(0,34,64,100),
+                                       labels=c("18-34", "35-64", "65+")),
+                          sex_recode=recode(sex_recode,"Men"="Male","Women"="Female"),
+                          race_eth=recode(race_eth,"Non-Hispanic Black"="Black",
+                                                   "Non-Hispanic White"="White",
+                                                   "Non-Hispanic Other"="Other",
+                                                   "Hispanic"="Hispanic"),
+                          group = paste(sex_recode, agecat, race_eth, sep="_"))
+  
+  sample <- function(test,NASdistributions){
+    test$prob <- runif(nrow(test))
+    rates <- NASdistributions %>% filter(group==unique(test$group))
+    test$drinkingstatus_detailed = ifelse(test$prob<=rates$cumsum[1], "Yearly drinker",
+                                          ifelse(test$prob>rates$cumsum[1] & test$prob<=rates$cumsum[2], "Former drinker",
+                                                 ifelse(test$prob>rates$cumsum[2] & test$prob<=rates$cumsum[3], "Lifetime abstainer",NA)))
+    return(test)
+  }
+  
+  datatoimpute <- data %>% filter(drinkingstatus==0)
+  datatoimpute <- datatoimpute %>% group_by(YEAR, State, group) %>% 
+    do(sample(.,NASdistributions)) %>% dplyr::select(-prob)
+  datatojoin <- data %>% filter(drinkingstatus!=0) %>% mutate(drinkingstatus_detailed="Monthly drinker")
+    
+  data <- rbind(datatojoin, datatoimpute) %>% 
+    mutate(drinkingstatus_updated = ifelse(drinkingstatus_detailed=="Yearly drinker", 1,
+                                           ifelse(drinkingstatus_detailed=="Monthly drinker", 1,
+                                                  ifelse(drinkingstatus_detailed=="Former drinker",0,
+                                                         ifelse(drinkingstatus_detailed=="Lifetime abstainer",0,NA)))))
+  return(data)
+}  
