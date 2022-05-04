90bc4bb5055260e3ec2eec1262fa549afe7eec39
charlottembuckley <c.m.buckley@sheffield.ac.uk>
2022-05-04 10:53:19 +0100

    updates from main branch

--- microsim/2_run_microsimulation/1_preprocessing_scripts/alcohol_transitions.R
+++ microsim/2_run_microsimulation/1_preprocessing_scripts/alcohol_transitions.R
@@ -41,24 +41,21 @@
 #   dplyr::select(cat, To, Probability) %>% group_by(cat) %>% 
 #   mutate(cumsum = cumsum(Probability))
 
-AlctransitionProbability <- readRDS("SIMAH_workplace/microsim/2_output_data/final_alc_transitionsUSA-1.RDS") %>% 
-  rename(To=StateTo)
-
-# AlctransitionProbability <- read.csv("SIMAH_workplace/microsim/1_input_data/AlcUse4, Ages7 2-yr TP.csv") %>% 
-#   mutate(sex = recode(sex, "Men"="m","Women"="f"),
-#          race = recode(race, "Hispanic"="SPA", "Black, non-Hispanic"="BLA",
-#                        "Other, non-Hispanic"="OTH","White, non-Hispanic"="WHI"),
-#          edu = recode(edu, "Low"="LEHS","Med"="SomeC","High"="College"),
-#          age_cat = gsub("Ages ", "", age_cat),
-#          From = recode(From, "Abstainer"="Lifetime abstainer", "Former"="Former drinker",
-#                        "Category I" = "Low risk", "Category II" = "Medium risk",
-#                        "Category III" = "High risk"),
-#          To = recode(To, "Abstainer"="Lifetime abstainer", "Former"="Former drinker",
-#                      "Category I" = "Low risk", "Category II" = "Medium risk",
-#                      "Category III" = "High risk")) %>%
-#   mutate(cat = paste(age_cat, sex, race, edu, From, sep="_")) %>% 
-#   dplyr::select(cat, To, Probability) %>% group_by(cat) %>% 
-#   mutate(cumsum = cumsum(Probability))
+AlctransitionProbability <- read.csv("SIMAH_workplace/microsim/1_input_data/AlcUse4, Ages7 2-yr TP.csv") %>% 
+  mutate(sex = recode(sex, "Men"="m","Women"="f"),
+         race = recode(race, "Hispanic"="SPA", "Black, non-Hispanic"="BLA",
+                       "Other, non-Hispanic"="OTH","White, non-Hispanic"="WHI"),
+         edu = recode(edu, "Low"="LEHS","Med"="SomeC","High"="College"),
+         age_cat = gsub("Ages ", "", age_cat),
+         From = recode(From, "Abstainer"="Lifetime abstainer", "Former"="Former drinker",
+                       "Category I" = "Low risk", "Category II" = "Medium risk",
+                       "Category III" = "High risk"),
+         To = recode(To, "Abstainer"="Lifetime abstainer", "Former"="Former drinker",
+                     "Category I" = "Low risk", "Category II" = "Medium risk",
+                     "Category III" = "High risk")) %>%
+  mutate(cat = paste(age_cat, sex, race, edu, From, sep="_")) %>% 
+  dplyr::select(cat, To, Probability) %>% group_by(cat) %>% 
+  mutate(cumsum = cumsum(Probability))
 
 # AlctransitionProbability <- read.csv("SIMAH_workplace/microsim/1_input_data/AlcUse5, Ages7, 2-yr TP.csv") %>% 
 #   mutate(sex = recode(sex, "Men"="m","Women"="f"),
--- nhis/SES x Lifestyle Manuscript/6_Sensitivity2 - Stratified by age.R
+++ nhis/SES x Lifestyle Manuscript/6_Sensitivity2 - Stratified by age.R
@@ -26,11 +26,9 @@ source("Function - CausalMed Results.R")
 
 
 # Load data
-nhis        <- readRDS (file.path(data, "nhis.rds"))
-nhis_male   <- readRDS (file.path(data, "nhis_male.rds"))
-nhis_female <- readRDS (file.path(data, "nhis_female.rds"))
-
-
+nhis        <- readRDS (paste0(data, "nhis25_85.rds"))
+nhis_male   <- filter(nhis, female==0)
+nhis_female <- filter(nhis, female==1)
 
 # Aalen Models Stratified by age (Objective 1) --------------------------------------------------------------------------------------------------
 
