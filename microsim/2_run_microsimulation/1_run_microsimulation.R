3cbbb662962914b29a8dff63d70ea9113d6b25b2
charlottembuckley <c.m.buckley@sheffield.ac.uk>
2022-06-13 10:56:38 +0100

    first push of microsim R package

--- microsim/2_run_microsimulation/1_run_microsimulation.R
+++ microsim/2_run_microsimulation/1_run_microsimulation.R
@@ -1,80 +1,14 @@
-#####Wrapper code for dynamic microsimulation
-rm(list = ls(all.names = TRUE)) #will clear all objects includes hidden objects.
-gc()
-suppressPackageStartupMessages(library("dplyr"))
-library(dplyr)
-library(knitr)
-library(ipfp)
-library(tidyr)
-library(janitor)
-library(stringr)
-library(reshape2)
-library(pbapply)
-library(ggplot2)
-library(gridExtra)
-library(readr)
-library(readxl)
-library(parallel)
-library(foreach)
-options(scipen=999)
-# set seed for reproducibility - IMPORTANT - DO NOT CHANGE
-# note - this also needs to be ran straight after R has been opened
-set.seed(42)
+#####SIMAH project 2022 - script for running SIMAH microsimulation model
 
-####EDIT ONLY BELOW HERE ### 
-###set working directory to the main "Microsimulation" folder in your directory 
-WorkingDirectory <- "~/Google Drive/SIMAH Sheffield/"
-# WorkingDirectory <- "/home/cbuckley/"
+rm(list = ls(all.names = TRUE)) #will clear all objects includes hidden objects.
+
+###set working directory to the main "SIMAH" folder in your directory 
+WorkingDirectory <- "~/Google Drive/SIMAH Sheffield"
 setwd(paste(WorkingDirectory))
 
-####which geography -  needs to be written as USA, California, Minnesota, New York, Texas, Tennessee
-SelectedState <- "USA"
+source("SIMAH_code/microsim/2_run_microsimulation/0_model_settings.R")
 
-####Size of population 
-PopulationSize <- 1000000
-
-# what proportion of the population does this represent - change to ifelse with all pop sizes when other states added 
-WholePopSize <- read.csv("SIMAH_workplace/microsim/1_input_data/fullpopcounts.csv") %>% 
-  filter(STATE==SelectedState)
-
-proportion <- PopulationSize/WholePopSize$total
-proportion <- ifelse(proportion>1,1,proportion)
-
-model <- "SIMAH"
-
-#####first read in and process all the necessary data files 
-source("SIMAH_code/microsim/2_run_microsimulation/1_preprocessing_scripts/load_files.R")
-# load in the education transitions data
-source("SIMAH_code/microsim/2_run_microsimulation/1_preprocessing_scripts/education_transitions.R")
-
-# load in the alcohol transitions data 
-source("SIMAH_code/microsim/2_run_microsimulation/1_preprocessing_scripts/alcohol_transitions.R")
-
-# load all functions for running the microsimulation - death rates, migration, transition education
-source("SIMAH_code/microsim/2_run_microsimulation/1_functions/apply_death_rates.R")
-source("SIMAH_code/microsim/2_run_microsimulation/1_functions/outward_migration.R")
-source("SIMAH_code/microsim/2_run_microsimulation/1_functions/inward_migration.R")
-source("SIMAH_code/microsim/2_run_microsimulation/1_functions/education_setup.R")
-source("SIMAH_code/microsim/2_run_microsimulation/1_functions/transition_ed.R")
-source("SIMAH_code/microsim/2_run_microsimulation/1_functions/transition_alcohol.R")
-
-# load the function for running the simulation
-source("SIMAH_code/microsim/2_run_microsimulation/1_functions/simulation.R")
-
-# switch on and off migration and deaths
-migrationdeaths <- 1
-
-# switch on and off education updates
-updatingeducation <- 1
-
-# switch on and off alcohol updates
-updatingalcohol <- 0
-
-Rates <- readRDS(paste("SIMAH_workplace/microsim/1_input_data/migration_rates/final_rates",SelectedState,".RDS",sep=""))
-Rates$agecat <- as.character(Rates$agecat)
-source("SIMAH_code/microsim/2_run_microsimulation/1_preprocessing_scripts/projecting_migration_and_deaths.R")
 Output <- list()
-
 Output <- run_microsim(1,1,basepop, outwardmigrants, inwardmigrants, deathrates, apply_death_rates,
                        updatingeducation, education_setup, transitionroles,
                        calculate_migration_rates, outward_migration, inward_migration, 
