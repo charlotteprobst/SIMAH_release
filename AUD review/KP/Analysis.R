2c83e2aaf5daf37608a229b72f96c0953db9e521
Klajdi Puka <klajdi.puka@gmail.com>
2022-11-03 14:38:11 -0400

    Update Analysis.R

--- AUD review/KP/Analysis.R
+++ AUD review/KP/Analysis.R
@@ -24,40 +24,22 @@ data    <- "C:/Users/klajd/OneDrive/SIMAH/SIMAH_workspace/AUD review/Data/"   # 
 output  <- "C:/Users/klajd/OneDrive/SIMAH/SIMAH_workspace/AUD review/"        # Location of figures/tables
 
 
-data1 <- read_xlsx (paste0(data, "Data_extraction_AUD_mortality.xlsx"))
-
-data2 <- data1 %>%
-  clean_names() %>% remove_empty(which = c("rows", "cols")) %>%
-  fill(id_study, .direction="down") %>%
-  group_by(id_study) %>% 
-    fill(c(first_author, year_published, study_size_n, exposure, outcome, outcome_categories), .direction="down") %>%
-  ungroup %>% 
-  filter(!str_detect(adjusted_or_or_rr_hr, "rate")) %>%
+# Load data
+data1 <- read_xlsx (paste0(data, "Analysis_data_AUD_mortality.xlsx")) %>%
   mutate(author_year = paste0(first_author, " (", year_published, ")"),
-         alc_daily_g = alcohol_mean_gr_day_calculated,
-         group = outcome_categories,
-         total_n = n_per_alcohol_category,
-         outcome_n = n_per_outcome_category,
-         RR = ifelse(!is.na(adjusted_or_or_rr_hr), adjusted_or_or_rr_hr, or_or_rr_hr),
-         RR = as.numeric(ifelse(RR=="Ref", 1, RR)), 
-         lowerRR = as.numeric(ifelse(ci_95_percent_lower=="Ref", 1, ci_95_percent_lower)),
-         upperRR = as.numeric(ifelse(ci_95_percent_upper=="Ref", 1, ci_95_percent_upper)),
-         logRR = log(RR),
-         loglowerRR = log(lowerRR),
-         logupperRR = log(upperRR),
-         se = ifelse(upperRR!=1 & lowerRR!=1, (logupperRR - loglowerRR)/3.92, NA),
-         inverse_se = 1/se) %>%
-  select(id_study, id_line, author_year, study_size_n, exposure, outcome, 
-         group, alc_daily_g, total_n, outcome_n, 
-         RR, lowerRR, upperRR, logRR, logupperRR, loglowerRR, se, inverse_se)
-  
+         log_RR = log(RR),
+         log_lowerRR = log(lowerRR),
+         log_upperRR = log(upperRR),
+         se = ifelse(RR==1 & lowerRR ==1 & upperRR==1, NA, (log_upperRR - log_lowerRR)/3.92),
+         inverse_se = 1/se)
 
 
-ggplot(data2, aes(alc_daily_g, logRR, size=inverse_se)) + 
-  geom_point (shape=1, colour="black") + scale_size_area(max_size=20)
+ggplot(data1, aes(alc_daily_g, log_RR, size=inverse_se)) + 
+  geom_point (shape=1, colour="black") + scale_size_area(max_size=20) + theme_bw()
 
 
-lin_mod <- dosresmeta(formula=logRR ~ alc_daily_g, id=id_study, type= type, se=se, cases=cases, n=n, data=data_bin)
+
+lin_mod <- dosresmeta(formula=log_RR ~ alc_daily_g, id=id_study, type="cc", se=se, cases=outcome_n, n=total_n, data=data1)
 summary(lin_bin)
 
 
