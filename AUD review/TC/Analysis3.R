e74bf6ccd0297ad00b7c3aacca1be3c84367e64e
Tessa Carr <carrt5@mcmaster.ca>
2022-12-08 13:34:35 -0500

    Update Analysis3.R

--- AUD review/TC/Analysis3.R
+++ AUD review/TC/Analysis3.R
@@ -5,30 +5,45 @@ library(readxl)
 library(meta)
 library(metafor)
 library(data.table)
+library(tidyr)
+library(ggplot2)
+library(grid)
+library(forestplot)
 
 install.packages("tidyverse")
 install.packages("meta")
 install.packages("metafor")
 install.packages("metagen")
 install.packages("metabin")
+install.packages("forestplot")
 
 #Name file location and output accordingly
 file_location <- "/Users/tessacarr/Downloads/Data Extraction/"
 
 # Load data
 data <- read_xlsx(paste0(file_location, "Analysis_data_AUDtoAUDmortality.xlsx"), na="")
-  col_types = c("numeric", "text", "numeric", "text", "numeric", 
-                "numeric", "numeric", "numeric", "numeric", "text", "text")
+  #col_types = c("numeric", "text", "numeric", "text", "numeric", 
+                #"numeric", "numeric", "numeric", "numeric", "text", "text")
+
+data_frame <- data.frame(data) %>%
+  unite(author_year, first_author:year_published, sep = ", " )
   
 #Meta-analysis of single incidence rates 
-m1 <- metarate(event = outcome_n, time = (person_years/1000), data = data)
+m1 <- metarate(event = outcome_n, time = (person_years/1000), data = data_frame)
 
 #Forest plot
-forest(m1, ref = , xlim = c(0, 15), leftcols = c("first_author", "group", 
-      "outcome_n", "time"), 
-      leftlabs = c("Author","Group", "Events", "Time"),
+forest(m1, ref = , xlim = c(0, 16), leftcols = c("author_year", "group", 
+      "outcome_n", "time"),                                  #columns on left side of plot
+      leftlabs = c("Study","Group", "Events", "Time"),       #labels on left side of plot
       comb.fixed = FALSE, 
       comb.random = TRUE, 
+      lty.random = 3,                                        #creates dotted line
+      lty.fixed = 3,                                         #creates dotted line
+      xlab = "Observed Mortality Rate per 1000PY",
+      fs.xlab = 8,                                           #font size of x axis label
+      xlab.pos = 7,                                          #position of x axis label
+      fs.axis = 8,                                           #font size of x axis numbers
+      #text.random = "Overall effect",                       # to label the pooled random effects estimate 
       #byvar = "Group", 
       #subgroup = 
       fontsize= 12
