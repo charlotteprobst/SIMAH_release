1c467bf8feaf5f5a033a176cff49cea0f7fec18e
caroki <carolin.kilian@mailbox.org>
2023-01-09 10:27:09 +0100

    BRFSS drink trend 2000-2020 by sex and SES

---
+++ brfss/3_categorical_to_continuous/drink trend by SES.R
@@ -0,0 +1,140 @@
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+## Project: SIMAH  
+## Title: Drinking trend BRFSS by SES 
+## Author: Carolin Kilian
+## Start Date: 09/01/2023
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+
+library(tidyverse)
+library(data.table)
+library(dplyr)
+library(Hmisc)
+
+# --------------------------------------------------------------------------------------
+
+rm(list = ls())
+setwd("/Users/carolinkilian/Desktop/SIMAH_workplace/")
+DATE <- 20230109
+
+dat <- data.table(readRDS("brfss/processed_data/BRFSS_upshifted_2000_2020_final.RDS"))
+
+# --------------------------------------------------------------------------------------
+
+# weighted confidence interval: lower bound
+wtd.lb <- function(x, w) {
+  n    <- length(x)
+  mean <- wtd.mean(x, w)
+  var  <- wtd.var(x, w)
+  se   <- sqrt(var)/sqrt(length(x))
+  lb   <- mean + qt(0.025,n-1)*se
+  return(lb)
+}
+
+# weighted confidence interval: upper bound
+wtd.ub <- function(x, w) {
+  n    <- length(x)
+  mean <- wtd.mean(x, w)
+  var  <- wtd.var(x, w)
+  se   <- sqrt(var)/sqrt(length(x))
+  ub   <- mean + qt(0.975,n-1)*se 
+return(ub)
+}
+
+# --------------------------------------------------------------------------------------
+
+# alcohol categories (simah definition)
+dat <- dat %>% 
+  mutate(AlcCAT.simah = factor(ifelse(sex_recode=="Male" & gramsperday_upshifted>0 &
+                                        gramsperday_upshifted<=40, "Low risk",
+                                      ifelse(sex_recode=="Female" & gramsperday_upshifted>0 &
+                                               gramsperday_upshifted<=20, "Low risk",
+                                             ifelse(sex_recode=="Male" & gramsperday_upshifted>40 &
+                                                      gramsperday_upshifted<=60, "Medium risk",
+                                                    ifelse(sex_recode=="Female" & gramsperday_upshifted>20 &
+                                                             gramsperday_upshifted<=40, "Medium risk",
+                                                           ifelse(sex_recode=="Male" & gramsperday_upshifted>60 &
+                                                                    gramsperday_upshifted<=100, "High risk",
+                                                                  ifelse(sex_recode=="Female" & gramsperday_upshifted>40 &
+                                                                           gramsperday_upshifted<=60, "High risk", NA)))))), 
+                               levels = c("Low risk", "Medium risk", "High risk")))
+
+dat[, AlcCAT.low := ifelse(AlcCAT.simah == "Low risk", 1, ifelse(AlcCAT.simah %like% "Medium|High", 0, NA))]
+dat[, AlcCAT.medium := ifelse(AlcCAT.simah == "Medium risk", 1, ifelse(AlcCAT.simah %like% "Low|High", 0, NA))]
+dat[, AlcCAT.high := ifelse(AlcCAT.simah == "High risk", 1, ifelse(AlcCAT.simah %like% "Low|Medium", 0, NA))]
+
+# exclude USA (?)
+dat <- copy(dat[State != "USA"])
+
+# --------------------------------------------------------------------------------------
+
+# time trend drinking categories by sex and SES
+
+ANY <- dat %>% group_by(YEAR, sex_recode, education_summary) %>% 
+                summarise(ANY = wtd.mean(drinkingstatus_upshifted, final_sample_weight),
+                          ANY.lb = wtd.lb(drinkingstatus_upshifted, final_sample_weight),
+                          ANY.ub = wtd.ub(drinkingstatus_upshifted, final_sample_weight)) %>% as.data.table
+
+LOW <- dat %>% filter(gramsperday_upshifted > 0) %>% group_by(YEAR, sex_recode, education_summary) %>% 
+               summarise(LOW = wtd.mean(AlcCAT.low, final_sample_weight),
+                         LOW.lb = wtd.lb(AlcCAT.low, final_sample_weight),
+                         LOW.ub = wtd.ub(AlcCAT.low, final_sample_weight)) %>% as.data.table
+
+MEDIUM <- dat %>% group_by(YEAR, sex_recode, education_summary) %>% 
+                  summarise(MEDIUM = wtd.mean(AlcCAT.medium, final_sample_weight),
+                            MEDIUM.lb = wtd.lb(AlcCAT.medium, final_sample_weight),
+                            MEDIUM.ub = wtd.ub(AlcCAT.medium, final_sample_weight)) %>% as.data.table
+
+HIGH <- dat %>% group_by(YEAR, sex_recode, education_summary) %>% 
+                summarise(HIGH = wtd.mean(AlcCAT.high, final_sample_weight),
+                          HIGH.lb = wtd.lb(AlcCAT.high, final_sample_weight),
+                          HIGH.ub = wtd.ub(AlcCAT.high, final_sample_weight)) %>% as.data.table
+
+drinkCAT <- merge(ANY, LOW, by = c("YEAR", "sex_recode", "education_summary"), all = T)
+drinkCAT <- merge(drinkCAT, MEDIUM, by = c("YEAR", "sex_recode", "education_summary"), all = T)
+drinkCAT <- merge(drinkCAT, HIGH, by = c("YEAR", "sex_recode", "education_summary"), all = T)
+
+ggplot(data = drinkCAT, aes(x = YEAR, y = ANY, color = as.factor(sex_recode))) +
+  geom_point() + geom_line() + geom_pointrange(aes(ymin = ANY.lb, ymax = ANY.ub)) +
+  facet_grid(rows = vars(factor(education_summary, levels = c("LEHS", "SomeC", "College"))), scale = "free") +
+  scale_y_continuous(labels = scales::percent, name = "Prevalence past-year alcohol use (weighted)") +
+  theme_bw() + theme(legend.position="bottom", legend.title=element_blank(), strip.background = element_rect(fill="white")) 
+ggsave(paste0('brfss/outputs/figures_trend/', DATE, '_ANY.png'), dpi=300, width = 12, height = 7)
+
+ggplot(data = drinkCAT, aes(x = YEAR, y = LOW, color = as.factor(sex_recode))) +
+  geom_point() + geom_line() + geom_pointrange(aes(ymin = LOW.lb, ymax = LOW.ub)) +
+  facet_grid(rows = vars(factor(education_summary, levels = c("LEHS", "SomeC", "College"))), scale = "free") +
+  scale_y_continuous(labels = scales::percent, name = "Prevalence low risk drinking (weighted)") +
+  theme_bw() + theme(legend.position="bottom", legend.title=element_blank(), strip.background = element_rect(fill="white")) 
+ggsave(paste0('brfss/outputs/figures_trend/', DATE, '_LOW RISK.png'), dpi=300, width = 12, height = 7)
+
+ggplot(data = drinkCAT, aes(x = YEAR, y = MEDIUM, color = as.factor(sex_recode))) +
+  geom_point() + geom_line() + geom_pointrange(aes(ymin = MEDIUM.lb, ymax = MEDIUM.ub)) +
+  facet_grid(rows = vars(factor(education_summary, levels = c("LEHS", "SomeC", "College"))), scale = "free") +
+  scale_y_continuous(labels = scales::percent, name = "Prevalence medium risk drinking (weighted)") +
+  theme_bw() + theme(legend.position="bottom", legend.title=element_blank(), strip.background = element_rect(fill="white")) 
+ggsave(paste0('brfss/outputs/figures_trend/', DATE, '_MEDIUM RISK.png'), dpi=300, width = 12, height = 7)
+
+ggplot(data = drinkCAT, aes(x = YEAR, y = HIGH, color = as.factor(sex_recode))) +
+  geom_point() + geom_line() + geom_pointrange(aes(ymin = HIGH.lb, ymax = HIGH.ub)) +
+  facet_grid(rows = vars(factor(education_summary, levels = c("LEHS", "SomeC", "College"))), scale = "free") +
+  scale_y_continuous(labels = scales::percent, name = "Prevalence high risk drinking (weighted)") +
+  theme_bw() + theme(legend.position="bottom", legend.title=element_blank(), strip.background = element_rect(fill="white")) 
+ggsave(paste0('brfss/outputs/figures_trend/', DATE, '_HIGH RISK.png'), dpi=300, width = 12, height = 7)
+
+# --------------------------------------------------------------------------------------
+
+# time trend GPD (alc-user only) by sex and SES
+
+GPD <- dat %>% filter(gramsperday_upshifted > 0) %>% group_by(YEAR, sex_recode, education_summary) %>% 
+               summarise(GPD.mean = wtd.mean(gramsperday_upshifted, final_sample_weight),
+                         GPD.lb = wtd.lb(gramsperday_upshifted, final_sample_weight),
+                         GPD.ub = wtd.ub(gramsperday_upshifted, final_sample_weight)) %>% as.data.table
+
+ggplot(data = GPD, aes(x = YEAR, y = GPD.mean, color = as.factor(sex_recode))) +
+  geom_point() + geom_line() + geom_pointrange(aes(ymin = GPD.lb, ymax = GPD.ub)) +
+  facet_grid(rows = vars(factor(education_summary, levels = c("LEHS", "SomeC", "College"))), scale = "free") +
+  labs(y = "Average grams per day, drinkers only (weighted)") +
+  theme_bw() + theme(legend.position="bottom", legend.title=element_blank(), strip.background = element_rect(fill="white")) 
+ggsave(paste0('brfss/outputs/figures_trend/', DATE, '_GPD drinkers.png'), dpi=300, width = 12, height = 7)
