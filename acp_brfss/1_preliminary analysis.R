6f5c741a712271d18d92427dad27052de29ee9a2
caroki <carolin.kilian@mailbox.org>
2023-01-11 15:49:16 +0100

    Updated version

--- acp_brfss/1_preliminary analysis.R
+++ acp_brfss/1_preliminary analysis.R
@@ -20,8 +20,29 @@ library(tidyverse)
 library(data.table)
 library(dplyr)
 library(openxlsx)
-library(lme4)
-library(gee)
+library(Hmisc)
+
+# --------------------------------------------------------------------------------------
+
+# weighted confidence interval: lower bound
+wtd.lb <- function(x, w) {
+  n    <- length(x)
+  mean <- wtd.mean(x, w)
+  var  <- wtd.var(x, w)
+  se   <- sqrt(var)/sqrt(length(x))
+  lb   <- mean + qt(0.025,n-1)*se
+  return(lb)
+}
+
+# weighted confidence interval: upper bound
+wtd.ub <- function(x, w) {
+  n    <- length(x)
+  mean <- wtd.mean(x, w)
+  var  <- wtd.var(x, w)
+  se   <- sqrt(var)/sqrt(length(x))
+  ub   <- mean + qt(0.975,n-1)*se 
+  return(ub)
+}
 
 # --------------------------------------------------------------------------------------
 
@@ -34,11 +55,7 @@ setwd("/Users/carolinkilian/Desktop/SIMAH_workplace/")
 DATE <- 20230103
 
 datBRFSS <- data.table(readRDS("brfss/processed_data/BRFSS_upshifted_2000_2020_final.RDS"))
-datBRFSS <- datBRFSS[State != "USA"]
-datBRFSS[datBRFSS$State == "DC"]$State <- "District of Columbia"
-  
-datAV <- data.table(read.xlsx("policies/LIBERAL_control_outlet_hours.xlsx", na.strings = c("NA", "")))
-datAV <- copy(datAV[year >= 2000,.(state, year, no_sun_sales, controlstate)])
+data <- datBRFSS[State %like% "California|Indiana|Pennsylvania|Tennessee|Texas"]
 
 # --------------------------------------------------------------------------------------
 
@@ -46,66 +63,113 @@ datAV <- copy(datAV[year >= 2000,.(state, year, no_sun_sales, controlstate)])
 # PREPARE / CHECK DATA
 # ----------------------------------------------------------------
 
-data <- merge(datBRFSS, datAV, by.x = c("State", "YEAR"), by.y = c("state", "year"), all.x = T)
-data[is.na(no_sun_sales), table(State, YEAR)]
-
 # cap estimates >200 GPD
 summary(data$gramsperday_orig)
 summary(data[gramsperday_upshifted > 200]$gramsperday_orig)
 data[, GPD_upshifted_capped := ifelse(gramsperday_upshifted > 200, 200, gramsperday_upshifted)]
+data[, GPD_orig_capped := ifelse(gramsperday_orig > 200, 200, gramsperday_orig)]
 
 # GPD distribution
 ggplot(data = data[data$GPD_upshifted_capped > 0]) +
   geom_histogram(aes(x = GPD_upshifted_capped, color = sex_recode), binwidth = 1)
 
-# Subgroups
-data[, sex_education := paste0(sex_recode, "_", education_summary)]
-data[, sex_raceth := paste0(sex_recode, "_", race_eth)]
-data[, sex_education_raceth := paste0(sex_recode, "_", education_summary, "_", race_eth)]
+# subgroups
+data[, education := factor(education_summary, levels = c("LEHS", "SomeC", "College"))]
 
-# GPD distribution by subgroups
-ggplot(data = data[data$GPD_upshifted_capped > 0]) +
-  geom_histogram(aes(x = GPD_upshifted_capped, color = as.factor(YEAR)), binwidth = 1) + 
-  facet_wrap(vars(sex_education_raceth), scales = "free")
+# drinking categories
+data <- data %>% 
+  mutate(AlcCAT.simah = factor(ifelse(sex_recode=="Male" & GPD_upshifted_capped>0 &
+                                        GPD_upshifted_capped<=40, "Low risk",
+                                      ifelse(sex_recode=="Female" & GPD_upshifted_capped>0 &
+                                               GPD_upshifted_capped<=20, "Low risk",
+                                             ifelse(sex_recode=="Male" & GPD_upshifted_capped>40 &
+                                                      GPD_upshifted_capped<=60, "Medium risk",
+                                                    ifelse(sex_recode=="Female" & GPD_upshifted_capped>20 &
+                                                             GPD_upshifted_capped<=40, "Medium risk",
+                                                           ifelse(sex_recode=="Male" & GPD_upshifted_capped>60 &
+                                                                    GPD_upshifted_capped<=100, "High risk",
+                                                                  ifelse(sex_recode=="Female" & GPD_upshifted_capped>40 &
+                                                                           GPD_upshifted_capped<=60, "High risk", 
+                                                                         ifelse(sex_recode=="Male" & GPD_upshifted_capped>100, "Very high risk",
+                                                                                ifelse(sex_recode=="Female" & GPD_upshifted_capped>60, "Very high risk", NA)))))))), 
+                               levels = c("Low risk", "Medium risk", "High risk", "Very high risk")))
 
-# GPD distribution by subgroups and state
-ggplot(data = data[data$GPD_upshifted_capped > 0 & race_eth %like% "Other"]) +
-  geom_histogram(aes(x = GPD_upshifted_capped, color = as.factor(sex_education)), binwidth = 1) + 
-  facet_wrap(vars(State), scales = "free")
+# quarterly data
+data[, Q := ifelse(surveymonth %like% "Feb|Mar|Apr", 1, ifelse(surveymonth %like% "May|Jun|Jul", 2,
+                   ifelse(surveymonth %like% "Aug|Sep|Oct", 3, ifelse(surveymonth %like% "Nov|Dec|Jan", 4, NA))))]
+data[, QYEAR := ifelse(surveymonth %like% "Jan", paste0(surveyyear-1, "/", Q), paste0(surveyyear, "/", Q))]
 
-data[GPD_upshifted_capped > 0, table(State, sex_education_raceth)]
+pdat <- copy(data[QYEAR != "1999/4" & QYEAR != "2021/1"]) # drop those interviewed in Jan 2000 (150) and 2021 (17)
+pdat[, QYEAR := as.factor(QYEAR)]
 
-# GPD distribution over time by subgroups
-ggplot(data = data[data$GPD_upshifted_capped > 0]) +
-  geom_smooth(aes(x = GPD_upshifted_capped, y = YEAR, color = as.factor(State))) + 
-  facet_wrap(vars(sex_education_raceth), scales = "free")
+# intervention year
+pdat[, INT_YEAR := ifelse(State %like% "California|Texas", NA, ifelse(State %like% "Pennsylvania", 2003, 
+                          ifelse(State %like% "Tennessee", 2018, ifelse(State %like% "Indiana", 2018, NA))))]
+pdat[, INT_QYEAR := ifelse(State %like% "California|Texas", NA, ifelse(State %like% "Pennsylvania", "2003/3", 
+                          ifelse(State %like% "Tennessee", "2018/2", ifelse(State %like% "Indiana", "2018/2", NA))))]
+
+# weighted mean consumption
+pdatSES <- pdat %>% group_by(State, YEAR, sex, education) %>% 
+  summarise(GPD.mean = wtd.mean(GPD_upshifted_capped, final_sample_weight),
+            GPD.lb = wtd.lb(GPD_upshifted_capped, final_sample_weight),
+            GPD.ub = wtd.ub(GPD_upshifted_capped, final_sample_weight),
+            INT_YEAR = mean(INT_YEAR)) %>% as.data.table
 
 # GPD distribution over time by state
-ggplot(data = data[data$GPD_upshifted_capped > 0]) +
-  geom_smooth(aes(x = GPD_upshifted_capped, y = YEAR, color = as.factor(sex_education_raceth))) + 
-  facet_wrap(vars(as.factor(State)), scales = "free")
+ggplot(data = pdatSES, aes(x = YEAR, y = GPD.mean, color = as.factor(education))) +
+  geom_point() + geom_line() + 
+  geom_pointrange(aes(ymin = GPD.lb, ymax = GPD.ub)) + 
+  geom_vline(aes(xintercept = INT_YEAR)) +
+  facet_grid(rows = vars(as.factor(State)), scales = "free") +
+  theme_bw() + theme(legend.position="bottom", legend.title=element_blank(), strip.background = element_rect(fill="white")) 
+ggsave(paste0('acp_brfss/outputs/figures/', DATE, '_annual TS_state.png'), dpi=300, width = 12, height = 7)
+
+# GPD distribution over time by group
+ggplot(data = pdatSES, aes(x = YEAR, y = GPD.mean, color = as.factor(State))) +
+  geom_line() + geom_pointrange(aes(ymin = GPD.lb, ymax = GPD.ub)) + 
+  facet_grid(rows = vars(as.factor(education)), scales = "free") +
+  theme_bw() + theme(legend.position="bottom", legend.title=element_blank(), strip.background = element_rect(fill="white")) 
+ggsave(paste0('acp_brfss/outputs/figures/', DATE, '_annual TS_education.png'), dpi=300, width = 12, height = 7)
+
+# GPD distribution over time (quarterly, unweighted data) by state
+pdat[, qyear := as.numeric(QYEAR)]
+pdat[, int_qyear := ifelse(INT_QYEAR == QYEAR, qyear, NA)]
+
+  # education
+  ggplot(data = pdat, aes(x = qyear, y = GPD_upshifted_capped, color = education)) +
+    geom_smooth() + geom_vline(aes(xintercept = int_qyear)) +
+    facet_grid(cols = vars(as.factor(State)), rows = vars(sex_recode), scales = "free") +
+    theme_bw() + theme(legend.position="bottom", legend.title=element_blank(), strip.background = element_rect(fill="white")) 
+  ggsave(paste0('acp_brfss/outputs/figures/', DATE, '_quartly TS (unweighted) education_state.png'), dpi=300, width = 12, height = 5)
+ 
+  # race ethnicity  
+  ggplot(data = pdat, aes(x = qyear, y = GPD_upshifted_capped, color = race_eth)) +
+    geom_smooth() + geom_vline(aes(xintercept = int_qyear)) +
+    facet_grid(cols = vars(as.factor(State)), rows = vars(sex_recode), scales = "free") +
+    theme_bw() + theme(legend.position="bottom", legend.title=element_blank(), strip.background = element_rect(fill="white")) 
+  ggsave(paste0('acp_brfss/outputs/figures/', DATE, '_quartly TS (unweighted) race ethn_state.png'), dpi=300, width = 12, height = 5)
+
+  # drinking categories
+  ggplot(data = pdat[GPD_upshifted_capped > 0], aes(x = qyear, y = GPD_upshifted_capped, color = sex_recode)) +
+    geom_smooth() + geom_vline(aes(xintercept = int_qyear)) +
+    facet_grid(cols = vars(as.factor(State)), rows = vars(AlcCAT.simah), scales = "free") +
+    theme_bw() + theme(legend.position="bottom", legend.title=element_blank(), strip.background = element_rect(fill="white")) 
+  ggsave(paste0('acp_brfss/outputs/figures/', DATE, '_quartly TS (unweighted) drink cat_state.png'), dpi=300, width = 12, height = 5)
+  
+  
+# ----------------------------------------------------------------
+# PRELIMINARY ANALYSIS
+# ----------------------------------------------------------------
 
 # prepare variables
 summary(data$age)
 data[, age_group := as.factor(ifelse(age_var < 35, 1, ifelse(age_var > 34 & age_var < 65, 2, ifelse(age_var > 64, 3, NA))))]
 data[, table(age_var, age_group)]
-
+  
 data[, no_sun_sales := as.factor(no_sun_sales)]
 data[, controlstate := as.factor(controlstate)]
 data[, marital_status := as.factor(marital_status)]
-
-data[GPD_upshifted_capped > 0, logGDP := log(GPD_upshifted_capped)]
-
-
-# ----------------------------------------------------------------
-# PRELIMINARY ANALYSIS - no tax data
-# ----------------------------------------------------------------
-
-ggplot(data = data[data$GPD_upshifted_capped > 0]) +
-  geom_histogram(aes(x = logGDP), binwidth = 1)
   
-lmer(logGDP ~ no_sun_sales + age_var + marital_status + controlstate + (1 + YEAR | State), 
-      data = data[GPD_upshifted_capped > 0], weights = final_sample_weight)
-
-
-
+data[GPD_upshifted_capped > 0, logGDP := log(GPD_upshifted_capped)]
+  
+  
