a5d4f9f20bf68150fa44959fbab81150a25783aa
Sophie Bright <serbright1@sheffield.ac.uk>
2023-05-02 15:48:54 +0200

    first draft of figure 1

--- psid/2_education_model/3_analyse_probability.R
+++ psid/2_education_model/3_analyse_probability.R
@@ -11,9 +11,12 @@ library(parallel)
 library(ggplot2)
 
 # setwd("/home/cbuckley")
-setwd("~/Google Drive/SIMAH Sheffield")
+# setwd("~/Google Drive/SIMAH Sheffield")
 
-TPs <- read.csv("SIMAH_workplace/education_transitions/final_models/income_model_TP_6cat.csv") %>% 
+# setwd Sophie
+setwd("C:/Users/cmp21seb/Documents/SIMAH/")
+
+TPs <- read.csv("SIMAH_workplace/education_transitions/final_models/income_model_TP_6cat_16.csv") %>% 
   mutate(racefinal=ifelse(racefinal=="white","White",
                           ifelse(racefinal=="black","Black",
                                  ifelse(racefinal=="hispanic","Hispanic",
@@ -23,7 +26,8 @@ TPs <- read.csv("SIMAH_workplace/education_transitions/final_models/income_model
          sex = ifelse(sex=="male","Men","Women"),
          incomequintile = paste0("Income quintile ", incomequintile)) %>% filter(incomequintile=="Income quintile 1" | incomequintile=="Income quintile 5") %>% 
   mutate(incomecat = ifelse(incomequintile=="Income quintile 1", "Lowest income quintile","Highest income quintile"))
-  
+
+# Transition LHS to HS at age 18
 ggplot(data=subset(TPs, age==18 & Transition=="HS->SomeC1"), aes(x=time, y=prob, fill=as.factor(incomecat))) + 
   geom_bar(stat="identity", position="dodge", colour="black") + facet_grid(cols=vars(racefinal), rows=vars(sex)) + theme_bw() +
   theme(legend.position="bottom",
@@ -31,13 +35,21 @@ ggplot(data=subset(TPs, age==18 & Transition=="HS->SomeC1"), aes(x=time, y=prob,
         strip.background=element_rect(fill="white",colour="black")) + xlab("Race and ethnicity") + ylab("Probability") + 
   ggtitle("High school or less -> 1 year college at age 18")
 
+# Transition HS to some college
+ggplot(data=subset(TPs, age==18 & Transition=="HS->SomeC1"), aes(x=time, y=prob, fill=as.factor(incomecat))) + 
+  geom_bar(stat="identity", position="dodge", colour="black") + facet_grid(cols=vars(racefinal), rows=vars(sex)) + theme_bw() +
+  theme(legend.position="bottom",
+        legend.title=element_blank(),
+        strip.background=element_rect(fill="white",colour="black")) + xlab("Race and ethnicity") + ylab("Probability") + 
+  ggtitle("High school or less -> 1 year college at age 18")
+
+# Transition some college to 4+ years college
 ggplot(data=subset(TPs, age==22 & Transition=="SomeC3->College"), aes(x=time, y=prob, fill=as.factor(incomecat))) + 
   geom_bar(stat="identity", position="dodge", colour="black") + facet_grid(cols=vars(racefinal), rows=vars(sex)) + theme_bw() +
   theme(legend.position="bottom",
         legend.title=element_blank(),
         strip.background=element_rect(fill="white",colour="black")) + xlab("Race and ethnicity") + ylab("Probability") + 
   ggtitle("3 years college -> College graduate at age 22")
-
 
 # most common pathway for each group
 commonpathways <- TPs %>% 
