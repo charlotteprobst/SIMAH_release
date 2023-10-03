5b455a1a80d1e90334686b9198f36595f515ae87
yachenz1 <yczhu92@gmail.com>
2023-10-03 13:59:55 -0700

    Different options for causal mediation analysis
    Removing exposure-mediator interactions.
    Using binary mediators (with/without survey weights).
    Using Cox models instead Aalen's models.

---
+++ nhis/Race x Lifestyle/No exposure-mediator interactions/Aalen without interaction/Binary mediators/with survey weights/5_Causal Mediation functions_updated_w survey weights.R
@@ -0,0 +1,369 @@
+# SIMAH Restricted-access Data
+## Functions for Causal Mediation 
+
+
+# Data preparation for Causal Mediation -----------------------------------------------------------------------------------
+
+# Function for data preparation (Race as exposure), to repeat analyses for men and women
+CMed_race3_prep <- function(data) {
+  
+  ### Step 0: Select data to use *****************************************************************************************************************
+  # **********************************************************************************************************************************************
+  mydata <- data %>%
+    mutate(
+      A.race = factor(ethnicity, levels=c(1,2,3), labels = c("White", "Black", "Hispanic")),
+      M1.alc = factor(alcohol2),
+      M2.smk = factor(smk2),
+      M3.bmi = factor(bmi2),
+      M4.phy = factor(phy2)) %>%
+    dplyr::select(A.race, M1.alc, M2.smk, M3.bmi, M4.phy, allcause_death, bl_age, end_age, married.factor, edu.factor, srvy_yr, new_psu, new_stratum, new_weight) %>%
+    filter(complete.cases(A.race, M1.alc, M2.smk, M3.bmi, M4.phy, allcause_death, bl_age, end_age, married.factor, edu.factor, srvy_yr, new_psu, new_stratum, new_weight))
+  
+  cat("Step 0 complete (Select data)", "\n") # progress indicator
+  
+  # NOTE: For technical reasons, the mediators should be coded as integers starting with 1
+  
+  
+  
+  ### Step 1: Fit a model for each mediator, , conditioning on exposure and all confounders *******************************************************
+  # ***********************************************************************************************************************************************
+  
+  # Fit model for each mediator, conditioning on exposure (race) and all confounders
+  
+  mydata$ATemp <- mydata$A.race # first, create and use a copy of the exposure variable (for technical reasons related to R)
+  mydata_svy <- mydata %>% as_survey_design(id = new_psu, strata = new_stratum, weights = new_weight, nest = TRUE)
+  
+  # fitM1 <- glm(M1.alc ~ ATemp + bl_age + married.factor + edu.factor + srvy_yr, data = mydata, family="binomial")
+  # fitM2 <- glm(M2.smk ~ ATemp + bl_age + married.factor + edu.factor + srvy_yr, data = mydata, family="binomial")
+  # fitM3 <- glm(M3.bmi ~ ATemp + bl_age + married.factor + edu.factor + srvy_yr, data = mydata, family="binomial")
+  # fitM4 <- glm(M4.phy ~ ATemp + bl_age + married.factor + edu.factor + srvy_yr, data = mydata, family="binomial")
+  
+  fitM1 <- svyglm(M1.alc ~ ATemp + bl_age + married.factor + edu.factor + srvy_yr, design = mydata_svy, family="binomial")  # 1 is the ref for mediators
+  fitM2 <- svyglm(M2.smk ~ ATemp + bl_age + married.factor + edu.factor + srvy_yr, design = mydata_svy, family="binomial")
+  fitM3 <- svyglm(M3.bmi ~ ATemp + bl_age + married.factor + edu.factor + srvy_yr, design = mydata_svy, family="binomial")
+  fitM4 <- svyglm(M4.phy ~ ATemp + bl_age + married.factor + edu.factor + srvy_yr, design = mydata_svy, family="binomial")
+  
+  cat("Step 1 complete (fit model for each mediator)", "\n")  # progress indicator
+  
+  
+  ### Step 2: Construct copies of ID and exposure *************************************************************************************************
+  # ***********************************************************************************************************************************************
+  
+  #Create ID Variable
+  mydata$ID <- 1:nrow(mydata) # construct id variable
+  
+  # Create counterfactual version of exposure (race); repeated 4 times because there are 4 mediators
+  levelsOfRACE <- unique(mydata$A.race)
+  myData1 <- mydata
+  myData2 <- mydata
+  myData3 <- mydata
+  myData1$race_M1.alc <- levelsOfRACE[1]
+  myData2$race_M1.alc <- levelsOfRACE[2]
+  myData3$race_M1.alc <- levelsOfRACE[3]
+  tempMyData <- rbind(myData1, myData2, myData3)
+  
+  myData1 <- tempMyData
+  myData2 <- tempMyData
+  myData3 <- tempMyData
+  myData1$race_M2.smk <- levelsOfRACE[1]
+  myData2$race_M2.smk <- levelsOfRACE[2]
+  myData3$race_M2.smk <- levelsOfRACE[3]
+  tempMyData <- rbind(myData1, myData2, myData3)
+  
+  myData1 <- tempMyData
+  myData2 <- tempMyData
+  myData3 <- tempMyData
+  myData1$race_M3.bmi <- levelsOfRACE[1]
+  myData2$race_M3.bmi <- levelsOfRACE[2]
+  myData3$race_M3.bmi <- levelsOfRACE[3]
+  tempMyData <- rbind(myData1, myData2, myData3)
+  
+  myData1 <- tempMyData
+  myData2 <- tempMyData
+  myData3 <- tempMyData
+  myData1$race_M4.phy <- levelsOfRACE[1]
+  myData2$race_M4.phy <- levelsOfRACE[2]
+  myData3$race_M4.phy <- levelsOfRACE[3]
+  newMyData <- rbind(myData1, myData2, myData3)
+  
+  
+  cat("Step 2 complete (duplicate data)", "\n")  # progress indicator
+  
+  
+  ### Step 3: Construct weights  *********************************************************************************************************************
+  # **************************************************************************************************************************************************
+  
+  # M1: alcohol
+  newMyData$ATemp <- newMyData$A.race
+  # newMyData_svy <- newMyData %>% as_survey_design(id = new_psu, strata = new_stratum, weights = new_weight, nest = TRUE)
+  temp <- as.vector(predict(fitM1,type = "response", newdata=newMyData))
+  tempDir1 <- ifelse(as.numeric(newMyData$M1.alc)-1, temp, 1-temp)  # M1.alc=1 (non-drinker) =TRUE is the reference group
+  #tempDir1 <- ifelse(as.numeric(newMyData$M1.alc), temp, 1-temp)
+  
+  newMyData$ATemp <- newMyData$race_M1.alc
+  temp <- as.vector(predict(fitM1,type = "response", newdata=newMyData))
+  tempIndir1 <- ifelse(as.numeric(newMyData$M1.alc)-1, temp, 1-temp) 
+  
+  newMyData$weight1 <- tempIndir1/tempDir1
+  
+  cat("Step 3.1 complete (Construct weights for alcohol)", "\n")  # progress indicator
+  
+  
+  #M2: Smoking
+  newMyData$ATemp <- newMyData$A.race
+
+  temp <- as.vector(predict(fitM2,type = "response", newdata=newMyData))
+  tempDir2 <- ifelse(as.numeric(newMyData$M2.smk)-1, temp, 1-temp)  # M1.alc=1 (non-drinker) =TRUE is the reference group
+  
+  newMyData$ATemp <- newMyData$race_M2.smk
+  temp <- as.vector(predict(fitM2,type = "response", newdata=newMyData))
+  tempIndir2 <- ifelse(as.numeric(newMyData$M2.smk)-1, temp, 1-temp) 
+  
+  newMyData$weight2 <- tempIndir2/tempDir2
+  
+  cat("Step 3.2 complete (Construct weights for smoking)", "\n")  # progress indicator
+  
+  
+  #M3: BMI
+  newMyData$ATemp <- newMyData$A.race
+  
+  temp <- as.vector(predict(fitM3,type = "response", newdata=newMyData))
+  tempDir3 <- ifelse(as.numeric(newMyData$M3.bmi)-1, temp, 1-temp)  # M1.alc=1 (non-drinker) =TRUE is the reference group
+  
+  newMyData$ATemp <- newMyData$race_M3.bmi
+  temp <- as.vector(predict(fitM3,type = "response", newdata=newMyData))
+  tempIndir3 <- ifelse(as.numeric(newMyData$M3.bmi)-1, temp, 1-temp) 
+  
+  newMyData$weight3 <- tempIndir3/tempDir3
+  
+  cat("Step 3.3 complete (Construct weights for BMI)", "\n")  # progress indicator
+  
+  
+  #M4: Physical activity
+  newMyData$ATemp <- newMyData$A.race
+  
+  temp <- as.vector(predict(fitM4,type = "response", newdata=newMyData))
+  tempDir4 <- ifelse(as.numeric(newMyData$M4.phy)-1, temp, 1-temp)  # M1.alc=1 (non-drinker) =TRUE is the reference group
+  
+  newMyData$ATemp <- newMyData$race_M4.phy
+  temp <- as.vector(predict(fitM4,type = "response", newdata=newMyData))
+  tempIndir4 <- ifelse(as.numeric(newMyData$M4.phy)-1, temp, 1-temp) 
+  
+  newMyData$weight4 <- tempIndir4/tempDir4
+  
+  cat("Step 3.4 complete (Construct weights for physical activity)", "\n")  # progress indicator
+  
+  
+  # Final weight
+  newMyData$weightM <- newMyData$weight1 * newMyData$weight2 * newMyData$weight3 * newMyData$weight4
+  
+  cat("Step 3 complete (final data complete)", "\n")  # progress indicator
+  
+  newMyData <- newMyData %>%
+    dplyr::select(ID, bl_age, end_age, allcause_death, A.race, race_M1.alc, race_M2.smk, race_M3.bmi, race_M4.phy, married.factor, edu.factor, srvy_yr, weightM) %>%
+    filter(complete.cases(ID, bl_age, end_age, allcause_death, A.race, race_M1.alc, race_M2.smk, race_M3.bmi, race_M4.phy, married.factor, edu.factor, srvy_yr, weightM))
+  
+  return(newMyData)
+}
+
+
+# Extract Data from Causal Mediation ----------------------------------------------------------------
+
+# Direct, indirect and mediated interactive effects and standard errors are derived directly from the summary() command below
+# Total effect is obtained by the sum of the three separate effects
+# Confidence intervals for total effects and mediated proportions are computed using the code below:
+
+# Function to get the (Not-Robust) total effect and proportion mediated
+getTE_NotRobust <- function(CMed_model, v){
+  TE <- sum(CMed_model$gamma[v])
+  mu <- CMed_model$gamma[v]
+  Omega <- CMed_model$var.gamma[v,v]    # To obtain non-robust estimates
+  temp <- mvrnorm(n=10^4, mu=mu, Sigma=Omega)
+  temp_TE <- apply(temp,1,sum)
+  med_prop <- c(mu/TE,1)
+  med_prop_CI <- rbind(t(apply(temp/temp_TE, 2, quantile, c(0.025, 0.975))), c(1,1))
+  output <- cbind(c(mu,TE), c(apply(temp,2,sd),sd(temp_TE)), med_prop, med_prop_CI)
+  colnames(output) <- c("Est.", "SE", "med_prop", "lowerCI", "UpperCI")
+  rownames(output) <- c(rownames(CMed_model$gamma)[v],"TE")
+  return(output)}
+
+        # # Function to get the (Robust) total effect and proportion mediated
+        getTE_Robust <- function(CMed_model, v){
+          TE <- sum(CMed_model$gamma[v])
+          mu <- CMed_model$gamma[v]
+          Omega <- CMed_model$robvar.gamma[v,v] # To obtain robust estimates
+          temp <- mvrnorm(n=10^4, mu=mu, Sigma=Omega)
+          temp_TE <- apply(temp,1,sum)
+          med_prop <- c(mu/TE,1)
+          med_prop_CI <- rbind(t(apply(temp/temp_TE, 2, quantile, c(0.025, 0.975))), c(1,1))
+          output <- cbind(c(mu,TE), c(apply(temp,2,sd),sd(temp_TE)), med_prop, med_prop_CI)
+          colnames(output) <- c("Est.", "SE", "med_prop", "lowerCI", "UpperCI")
+          rownames(output) <- c(rownames(CMed_model$gamma)[v],"TE")
+          return(output)}
+
+
+# Function to get the (Not-Robust) total combined indirect effect  
+getIE_NotRobust <- function(CMed_model, v){
+  IE <- sum(CMed_model$gamma[v])
+  mu <- CMed_model$gamma[v]
+  Omega <- CMed_model$var.gamma[v,v] # To obtain non-robust estimates
+  require(MASS)
+  temp <- mvrnorm(n=10^4, mu=mu, Sigma=Omega)
+  temp_IE <- apply(temp,1,sum)
+  med_prop <- c(mu/IE,1)
+  med_prop_CI <- rbind(t(apply(temp/temp_IE, 2, quantile, c(0.025, 0.975))), c(1,1))
+  output <- cbind(c(mu,IE), c(apply(temp,2,sd),sd(temp_IE)), med_prop, med_prop_CI)
+  colnames(output) <- c("Est.", "SE", "med_prop", "lowerCI", "UpperCI")
+  rownames(output) <- c(rownames(CMed_model$gamma)[v],"IE")
+  return(output)}
+
+
+        # Function to get the (Robust) total combined indirect effect  
+        getIE_Robust <- function(CMed_model, v){
+          IE <- sum(CMed_model$gamma[v])
+          mu <- CMed_model$gamma[v]
+          Omega <- CMed_model$robvar.gamma[v,v] # To obtain robust estimates
+          require(MASS)
+          temp <- mvrnorm(n=10^4, mu=mu, Sigma=Omega)
+          temp_IE <- apply(temp,1,sum)
+          med_prop <- c(mu/IE,1)
+          med_prop_CI <- rbind(t(apply(temp/temp_IE, 2, quantile, c(0.025, 0.975))), c(1,1))
+          output <- cbind(c(mu,IE), c(apply(temp,2,sd),sd(temp_IE)), med_prop, med_prop_CI)
+          colnames(output) <- c("Est.", "SE", "med_prop", "lowerCI", "UpperCI")
+          rownames(output) <- c(rownames(CMed_model$gamma)[v],"IE")
+          return(output)}
+
+
+# Function to get the (Not Robust)  proportion mediated of the combined indirect effect
+getTE_IE_NotRobust <- function(CMed_model, v, z){
+  #total effect
+  TE <- sum(CMed_model$gamma[v])
+  mu <- CMed_model$gamma[v]
+  # Omega <- CMed_model$robvar.gamma[v,v]  # To obtain robust estimates
+  Omega <- CMed_model$var.gamma[v,v]     # To obtain non-robust estimates
+  require(MASS)
+  temp <- mvrnorm(n=10^4, mu=mu, Sigma=Omega)
+  temp_TE <- apply(temp,1,sum)
+  IE <- sum(CMed_model$gamma[z])
+  muIE <- CMed_model$gamma[z]
+  #OmegaIE <- CMed_model$robvar.gamma[z,z] # To obtain robust estimates
+  OmegaIE <- CMed_model$var.gamma[z,z]    # To obtain non-robust estimates
+  require(MASS)
+  tempIE <- mvrnorm(n=10^4, mu=muIE, Sigma=OmegaIE)
+  temp_IE <- apply(tempIE,1,sum)
+  med_prop <- c(IE/TE,1)
+  med_prop_CI <- (temp_IE/temp_TE)
+  output <- cbind(IE, med_prop, quantile)
+  quantile <- quantile(med_prop_CI, c(0.025, 0.975))
+  output <- cbind(IE, med_prop, quantile)
+  return(output)}
+
+
+      # Function to get the (Not Robust)  proportion mediated of the combined indirect effect
+      getTE_IE_Robust <- function(CMed_model, v, z){
+        #total effect
+        TE <- sum(CMed_model$gamma[v])
+        mu <- CMed_model$gamma[v]
+        Omega <- CMed_model$robvar.gamma[v,v]  # To obtain robust estimates
+        require(MASS)
+        temp <- mvrnorm(n=10^4, mu=mu, Sigma=Omega)
+        temp_TE <- apply(temp,1,sum)
+        IE <- sum(CMed_model$gamma[z])
+        muIE <- CMed_model$gamma[z]
+        OmegaIE <- CMed_model$robvar.gamma[z,z] # To obtain robust estimates
+        require(MASS)
+        tempIE <- mvrnorm(n=10^4, mu=muIE, Sigma=OmegaIE)
+        temp_IE <- apply(tempIE,1,sum)
+        med_prop <- c(IE/TE,1)
+        med_prop_CI <- (temp_IE/temp_TE)
+        output <- cbind(IE, med_prop, quantile)
+        quantile <- quantile(med_prop_CI, c(0.025, 0.975))
+        output <- cbind(IE, med_prop, quantile)
+        return(output)}
+
+      
+      
+# Format Data from Causal Mediation ----------------------------------------------------------------
+
+format_CMed <- function (model, coef_list) {
+  
+  group <- enexpr(coef_list)
+  
+  # All_coef model coefficients   
+  All_coef <- coef(model) %>%
+    as.data.frame() %>% 
+    rownames_to_column(var = "variable") %>% 
+    mutate (deaths_10000py = round(Coef. * 10000, 1),
+      lower = round(`lower2.5%` * 10000, 1),
+      upper = round(`upper97.5%` * 10000, 1),
+      deaths_10000py_CI = paste0(deaths_10000py, " (", lower, ", ", upper, ")")) %>% 
+    dplyr::select (variable, deaths_10000py_CI)
+  
+  coef <- slice(All_coef, coef_list)
+  
+  
+  # Function to get the total effect and proportion mediated
+  TE_prop <- getTE_NotRobust(model, coef_list) %>% 
+    as.data.frame() %>% rownames_to_column(var = "variable") 
+  
+  TE <- filter(TE_prop, variable =="TE") %>% 
+    mutate (lower = round((`Est.` - (1.96 * SE))*10000,1), 
+      upper = round((`Est.` + (1.96 * SE))*10000,1), 
+      deaths_10000py = round(`Est.` * 10000,1),
+      deaths_10000py_CI = paste0(deaths_10000py, " (", lower, ", ", upper, ")")) %>%
+    dplyr::select(variable, deaths_10000py_CI)
+  
+  prop <- filter(TE_prop, variable !="TE") %>%
+    mutate(prop = round(med_prop * 100,0),
+      lower = round(lowerCI * 100,0),
+      upper = round(UpperCI * 100,0),
+      prop_CI = paste0(prop, " (", lower, ", ", upper, ")")) %>% 
+    dplyr::select(variable, prop_CI) 
+  
+  
+  # Function to get the total combined indirect effect  (coef_list excluding 1st item)
+  IE <- getIE_NotRobust(model, coef_list[-1]) %>% 
+    as.data.frame() %>% rownames_to_column(var = "variable") %>% 
+    filter (variable == "IE") %>% 
+    mutate (lower = round((`Est.` - (1.96 * SE))*10000,1), 
+      upper = round((`Est.` + (1.96 * SE))*10000,1), 
+      deaths_10000py = round(`Est.` * 10000,1),
+      deaths_10000py_CI = paste0(deaths_10000py, " (", lower, ", ", upper, ")")) %>%
+    dplyr::select(variable, deaths_10000py_CI) 
+  
+  
+  # Function to get the proportion mediated of the combined indirect effect
+  IE_prop <- getTE_IE_NotRobust(model, coef_list, coef_list[-1]) %>% 
+    as.data.frame() %>% rownames_to_column(var = "variable") %>%
+    pivot_wider(names_from="variable", values_from=c("IE", "med_prop", "quantile")) %>%
+    mutate (IE_prop = round(`med_prop_2.5%` * 100, 0),
+      lower = round(`quantile_2.5%` *100, 0), 
+      upper = round(`quantile_97.5%` *100, 0),
+      prop_CI = paste0(IE_prop, " (", lower, ", ", upper, ")"),
+      variable = "IE") %>% 
+    dplyr::select(variable, prop_CI) 
+  
+  
+  one <- full_join(coef, prop, by="variable") %>%
+    mutate(label = c(paste0("02 Direct effect of ", group),
+      "04 Alcohol use: mediated",
+      "05 Smoking: mediated",
+      "06 BMI: mediated",
+      "07 Physical activity: mediated")) %>% 
+    dplyr::select(label, deaths_10000py_CI, prop_CI) 
+  
+  two <- TE %>%
+    mutate (label = ifelse(variable == "TE", paste0("01 Total effect of ", group), NA),
+      prop_CI = "100") %>% 
+    dplyr::select(label, deaths_10000py_CI, prop_CI)
+  
+  three <- full_join(IE, IE_prop, by="variable") %>%
+    mutate (label = ifelse(variable == "IE", paste0("03 Indirect effect of ", group), NA)) %>% 
+    dplyr::select(label, deaths_10000py_CI, prop_CI) 
+  
+  
+  final <- rbind(one, two, three) %>%
+    arrange(label) 
+  
+  return(final)
+}
---
+++ nhis/Race x Lifestyle/No exposure-mediator interactions/Cox without interaction/5_Causal Mediation analysis_updated Cox.R
@@ -0,0 +1,180 @@
+# SIMAH Restricted-access Data
+# Causal Mediation  
+
+
+# LOAD DATA AND SET FILE LOCATIONS
+
+# load libraries
+library(tidyverse)  # data management
+library(timereg)    # additive survival models
+library(VGAM)       # multinomial regression, needed for causal mediation
+library(MASS)       # needed for causal mediation functions
+library(knitr)      # formatted table
+library(survey)
+library(srvyr)
+library(broom)
+
+# Specify the data and output file locations
+
+data <- "~/Dropbox/Mac/Desktop/Public Health Institute/Alcohol Research Group/SIMAH/SIMAH_workplace/nhis/Restricted access data/Data/"
+output <- "~/Dropbox/Mac/Desktop/Public Health Institute/Alcohol Research Group/SIMAH/SIMAH_workplace/nhis/Restricted access data/Causal mediation/Race x Lifestyle/CausMed/18-85 years old/Extensions/"
+
+source("5_Causal Mediation functions.R")
+
+ 
+# Load data
+nhis18_85 <- readRDS(file.path(data, "nhis18_85.rds"))
+nhis18_male <- readRDS(file.path(data, "nhis18_male.rds")) 
+nhis18_female <- readRDS(file.path(data, "nhis18_female.rds"))
+
+nhis18_85_svy <- nhis18_85 %>% as_survey_design(id = new_psu, strata = new_stratum, weights = new_weight, nest = TRUE)
+
+# check the interaction effect between the exposure (ethnicity) and mediator (alcohol)
+cox_int <- svycoxph(Surv(bl_age, end_age, allcause_death) ~ ethnicity.factor * alcohol5v2.factor + female + married.factor + edu.factor + srvy_yr, design = nhis18_85_svy)
+
+cox_int_results <- cox_int %>% tidy(exponentiate = TRUE, conf.int = TRUE) %>% 
+  mutate(variable = term,
+         HR = round(estimate, 2),
+         conf.low = round(conf.low, 2),
+         conf.high = round(conf.high, 2),
+         p.value_HR = round(p.value, 3),
+         p.value_HR = ifelse(p.value_HR <.001, "<.001", p.value_HR),
+         CI = paste0("(",conf.low,", ", conf.high, ")")) %>%
+  select(variable, HR, CI, p.value_HR) %>%
+  #filter(str_detect(variable, "SES|lifestyle")) %>%
+  # mutate(variable = str_remove(variable, fixed("SES")),   # keep the name in order to calculate RERI
+  #        variable = str_remove(variable, fixed("lifestyle"))) %>%
+  add_row(variable = "INTERACTION MODELS", .before=1)
+
+
+library(gmodels)
+# Build a crosstable between admit and rank
+CrossTable(nhis18_85$alcohol5v2.factor, nhis18_85$smk.factor, expected = T, prop.r = T, prop.c = T, prop.chisq = T, sresid = T)
+
+
+library(nnet)
+m1 <- multinom(alcohol5v2.factor ~ smk.factor + female + married.factor + edu.factor + srvy_yr, data = nhis18_85)
+m1_results <- tidy(m1, exponentiate = T, conf.int = T) %>% data.frame() 
+m1_results %>% mutate_at(c("estimate", "std.error", "statistic", "conf.low", "conf.high"), list(~round(., digits = 2)) ) %>%
+  mutate(p.value = case_when(p.value < 0.001 ~ "<0.001",
+                             p.value < 0.01 & p.value >= 0.001 ~ "<0.01",
+                             p.value < 0.05 & p.value >= 0.01 ~ "<0.05",
+                             p.value >= 0.05 ~ "insignificant")) %>%
+  select(-std.error, -statistic) %>%
+  rename(OR = estimate)
+
+
+
+aalen_int <- aalen(Surv(bl_age, end_age, allcause_death) ~ const(ethnicity)*const(alcohol5v2) + const(female) + const(married.factor) + 
+                     const(edu.factor) + const(srvy_yr), data = nhis18_85)
+
+aalen_int_results <- as.data.frame(cbind(aalen_int$gamma, diag(aalen_int$robvar.gamma))) %>%
+  mutate (variable = rownames(.),
+          var = V2,
+          p.value_Deaths = round(2*pnorm(-abs(estimate / sqrt(var))),3),
+          p.value_Deaths = ifelse(p.value_Deaths <.001, "<.001", p.value_Deaths),
+          lower.ci = round((estimate - (1.96 * sqrt(var)))*100000, 1),
+          upper.ci = round((estimate + (1.96 * sqrt(var)))*100000, 1),
+          estimate_100000py = round(estimate*100000, 1),
+          Deaths_CI_100000py = paste0("(",lower.ci,", ", upper.ci, ")")) %>%
+  select (variable, estimate_100000py, Deaths_CI_100000py, p.value_Deaths) %>%
+  filter(str_detect(variable, "SES|lifestyle")) %>%
+  mutate(variable = str_remove(variable, fixed("const(SES)")),
+         variable = str_remove(variable, fixed("const(lifestyle)"))) %>%
+  add_row(variable = "INTERACTION MODELS", .before=1)%>%
+  remove_rownames()
+
+# OBJECTIVE 2: Causal Mediation
+
+# The causal mediation analyses involves four steps:
+# 1) Fit separate multinomial logistic regressions with each mediator (M1, M2, M3, and M4) as the outcome.
+# 2) Create copies of the dataset to account for all possible combinations of the exposure and mediators. 
+# 3) Using the expanded dataset, calculate weights for each mediator using the predicted probabilities from Step 1. 
+# 4) Fit a marginal structural model using Aalen additive hazards with the weight as weight and the id as a cluster level; this ensures thatrobust standard errors are calculated. The model with robust variance and resampling (robust=TRUE) was not used because of computation limiations.  
+
+# For more details and theoretical justification/description see:
+# Lange et al. 2014 https//doi.org/10.1093/aje/kwt270
+# Lange et al. 2012 https//doi.org/10.1093/aje/kwr525
+# Lange et al. 2011 https//doi.org/10.1097/EDE.0b013e31821c680c
+
+
+# Data Preparation ------------------------------------------------------------------------------------------------------------------
+CMed_race3_prep(nhis18_female) %>% saveRDS(paste0(output, "expandedData_fem.rds"))
+CMed_race3_prep(nhis18_male)   %>% saveRDS(paste0(output, "expandedData_male.rds"))
+
+
+# Run Analyses, WOMEN ----------------------------------------------------------------------------------------------------------------
+
+# Load data
+expandedData <- readRDS(file.path(output, "expandedData_fem.rds")) %>%
+  filter(complete.cases(ID, bl_age, end_age, allcause_death, A.race, race_M1.alc, race_M2.smk, race_M3.bmi, race_M4.phy,
+                        married.factor, edu.factor, srvy_yr, weightM))
+
+hist(expandedData$weightM)
+
+# Run model
+CMed_f <- coxph(Surv(bl_age, end_age, allcause_death) ~ A.race + race_M1.alc + race_M2.smk + race_M3.bmi + race_M4.phy +
+                                                        married.factor + edu.factor + srvy_yr,  # adjust for survey year as continuous variable 
+                          data=expandedData, weights=expandedData$weightM, id=expandedData$ID)  
+                
+saveRDS(CMed_f, file.path(output, "CMed_f_cox.rds")) # Save model results      
+
+tidy(CMed_f, exponentiate = T, conf.int = T)
+
+model_coefficients <- coef(CMed_f) %>%
+  row.names() %>% 
+  as.data.frame() %>%
+  rownames_to_column()
+
+print("model coefficients"); print(model_coefficients)
+print("Black"); print(Black)
+print("Hispanic"); print(Hispanic)
+
+
+# Load model and view results
+CMed_model <- readRDS(file.path(output, "CMed_f_cox.rds"))  # load model (if needed)
+Black <- c(1, 3, 5, 7, 9)             # List the coefficients of interest 
+Hispanic <- c(2, 4, 6, 8, 10)  
+format_CMed(CMed_model, Black) %>% kable()    # print formatted results
+
+CMed_women_black <- format_CMed (CMed_model, Black)
+CMed_women_hispanic <- format_CMed (CMed_model, Hispanic)
+CMed_women <- rbind(CMed_women_black, CMed_women_hispanic)
+
+
+
+# Run Analyses, MEN ----------------------------------------------------------------------------------------------------------------
+
+# Load data
+expandedData <- readRDS(file.path(output, "expandedData_male.rds"))
+
+
+# Run Model
+CMed_m <- aalen(Surv(bl_age, end_age, allcause_death) ~ const(A.race) + const(race_M1.alc) + const(race_M2.smk) + const(race_M3.bmi) + const(race_M4.phy) +
+                const(married.factor) + const(edu.factor) + const(srvy_yr),  
+                data=expandedData, weights=expandedData$weightM, clusters=expandedData$ID, robust=0)
+
+saveRDS(CMed_m, file.path(output, "CMed_m.rds"))  # Save model results     
+
+# Load model and view results
+CMed_model <- readRDS(file.path(output, "CMed_m.rds"))  # load model (if needed)
+Black <- c(1, 3, 5, 7, 9, 13, 17, 21, 25)             # List the coefficients of interest 
+Hispanic <- c(2, 4, 6, 8, 10, 16, 20, 24, 28)  
+format_CMed (CMed_model, Black) %>% kable()    # print formatted results
+format_CMed (CMed_model, Hispanic) %>% kable() 
+
+CMed_men_black <- format_CMed (CMed_model, Black)
+CMed_men_hispanic <- format_CMed (CMed_model, Hispanic)
+CMed_men <- rbind(CMed_men_black, CMed_men_hispanic)
+
+
+# COMBINE Results
+
+colnames(CMed_men)   <- paste0("men_", colnames(CMed_men))
+colnames(CMed_women) <- paste0("women_", colnames(CMed_women))
+CMed_table <- cbind(CMed_men, CMed_women) %>% rename(term = men_label) %>% 
+  mutate(race = rep(c("Black", "Hispanic"), each = 11) ) %>%
+  relocate(race) %>% dplyr::select(-women_label)
+  
+CMed_table
+write.csv(CMed_table, file=paste0(output, "Table2 Causal Mediation results.csv")) # save results
