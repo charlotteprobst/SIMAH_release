35f1533154b396b0474fd3b3db61cb85de71ea66
charlottembuckley <c.m.buckley@sheffield.ac.uk>
2023-11-15 12:00:15 +0000

    code to resample from priors

--- microsim/2_run_microsimulation/education_transitions_calibration/plot_priors.R
+++ microsim/2_run_microsimulation/education_transitions_calibration/plot_priors.R
@@ -9,30 +9,36 @@ DataDirectory <- paste0(WorkingDirectory, "SIMAH_workplace/microsim/2_output_dat
 # load in microsim R package
 setwd(paste(WorkingDirectory))
 
-data <- read.csv(paste0(DataDirectory, "/prior_range_uninflated_newn.csv")) %>% 
+data <- read.csv(paste0(DataDirectory, "/prior_range_uninflated_neworder.csv")) %>% 
   mutate(AGECAT = cut(microsim.init.age,
                       breaks=c(0,24,34,44,54,64,79),
                       labels=c("18-24","25-34","35-44","45-54",
-                               "54-64","65-79")),
+                               "55-64","65-79")),
          SEX = ifelse(microsim.init.sex=="m", "Men","Women"),
          RACE = recode(microsim.init.race, "BLA"="Black","WHI"="White","SPA"="Hispanic",
                        "OTH"="Others")) %>% 
   rename(EDUC=microsim.init.education, YEAR=year) %>% 
-  group_by(YEAR, samplenum, seed, SEX, AGECAT, RACE,
+  group_by(YEAR, samplenum, seed, SEX, AGECAT,RACE,
            EDUC) %>% 
   summarise(n=sum(n)) %>% 
   ungroup() %>% 
-  group_by(YEAR, samplenum, SEX, AGECAT, RACE, EDUC) %>% 
+  group_by(YEAR, samplenum, SEX, AGECAT, EDUC,RACE) %>% 
   summarise(n=mean(n)) %>% 
   ungroup() %>% 
-  group_by(YEAR, samplenum, SEX, AGECAT, RACE) %>% 
+  group_by(YEAR, samplenum, SEX, AGECAT,RACE) %>% 
   mutate(prop=n/sum(n), YEAR=as.integer(YEAR)) %>% 
   dplyr::select(-n) %>% drop_na()
 
 # read in target data 
 targets <- read.csv(paste0(DataDirectory, "/target_data.csv")) %>% 
-  dplyr::select(-n) %>% 
-  rename(target=prop)
+  group_by(YEAR,SEX,AGECAT,EDUC,RACE) %>% 
+  summarise(n=sum(n)) %>% 
+  ungroup() %>% group_by(YEAR,SEX,AGECAT,RACE) %>% 
+  mutate(target=n/sum(n))
+  
+  # 
+  # dplyr::select(-n) %>% 
+  # rename(target=prop)
 
 data <- left_join(data,targets) %>% 
   mutate(EDUC = factor(EDUC, levels=c("LEHS","SomeC","College")))
@@ -47,7 +53,7 @@ ggplot(data=subset(data, SEX=="Men" & AGECAT=="18-24"),
   ggtitle("Men, 18-24 non-inflated prior") + 
   xlab("Year") + ylim(0,1)
 
-ggsave(paste0(DataDirectory, "/plot_prior_men_newn.png"), dpi=300, width=33, height=19, units="cm")
+ggsave(paste0(DataDirectory, "/plot_prior_men_new.png"), dpi=300, width=33, height=19, units="cm")
 
 ggplot(data=subset(data, SEX=="Women" & AGECAT=="18-24"), 
        aes(x=as.numeric(YEAR), y=prop, colour=as.factor(samplenum))) + 
@@ -59,7 +65,19 @@ ggplot(data=subset(data, SEX=="Women" & AGECAT=="18-24"),
   ggtitle("Women, 18-24 non-inflated prior") + 
   xlab("Year") + ylim(0,1)
 
-ggsave(paste0(DataDirectory, "/plot_prior_women_newn.png"), dpi=300, width=33, height=19, units="cm")
+ggsave(paste0(DataDirectory, "/plot_prior_women_new.png"), dpi=300, width=33, height=19, units="cm")
+
+ggplot(data=subset(data, SEX=="Men" & AGECAT=="18-24"), 
+       aes(x=as.numeric(YEAR), y=prop, colour=as.factor(samplenum))) + 
+  geom_line(linewidth=1) + 
+  geom_line(aes(x=YEAR,y=target), colour="darkblue",linewidth=1) + 
+  facet_grid(cols=vars(RACE), rows=vars(EDUC)) + 
+  theme_bw() + 
+  theme(legend.position = "none") + 
+  ggtitle("Men, 18-24 non-inflated prior") + 
+  xlab("Year") + ylim(0,1)
+
+ggsave(paste0(DataDirectory, "/plot_prior_men_newn.png"), dpi=300, width=33, height=19, units="cm")
 
 # calculate implausibility 
 implausibility <- data %>% 
@@ -74,8 +92,8 @@ maxmean <- implausibility %>%
             meanimplausibility = mean(implausibility)) %>% 
   ungroup() %>% 
   mutate(
-            ntilemax = ntile(maximplausibility, 1000),
-            ntilemean = ntile(meanimplausibility, 1000)) 
+            ntilemax = ntile(maximplausibility, 500),
+            ntilemean = ntile(meanimplausibility, 500)) 
 
 max <- data %>% 
   filter(samplenum %in% subset(maxmean, ntilemax<=3)$samplenum | 
