5e3e3f62d6fc3673653d9fb0cee190eaae00cbac
charlottembuckley <c.m.buckley@sheffield.ac.uk>
2023-12-18 15:45:34 +0000

    dose response curve code

--- microsimpackage/R/AUD_interaction.R
+++ microsimpackage/R/AUD_interaction.R
@@ -6,6 +6,8 @@
 #' @examples
 #' AUD including SES interaction effects
 AUDInteraction <- function(data,lhs){
+  data$microsim.init.alc.gpd <- ifelse(data$microsim.init.alc.gpd>=150, 150,
+                                       data$microsim.init.alc.gpd)
   data <- data %>%
     mutate(ageCAT = cut(microsim.init.age,
                         breaks=c(0,24,34,44,54,64,74,79),
@@ -23,7 +25,7 @@ AUDInteraction <- function(data,lhs){
   B_AUD_SomeCxGPD_MEN <- as.numeric(lhs["B_AUD_SomeCxGPD_MEN"])
   B_AUD_LEHSxGPD_WOMEN <- as.numeric(lhs["B_AUD_LEHSxGPD_WOMEN"])
   B_AUD_SomeCxGPD_WOMEN <- as.numeric(lhs["B_AUD_SomeCxGPD_WOMEN"])
-  
+
   #Former drinkers
   AUD_FD_LEHS_MEN <- as.numeric(lhs["AUD_FD_LEHS_MEN"])
   AUD_FD_SomeC_MEN <- as.numeric(lhs["AUD_FD_SomeC_MEN"])
@@ -35,7 +37,7 @@ AUDInteraction <- function(data,lhs){
   AUD_SomeCxFD_MEN <- as.numeric(lhs["AUD_SomeCxFD_MEN"])
   AUD_LEHSxFD_WOMEN <- as.numeric(lhs["AUD_LEHSxFD_WOMEN"])
   AUD_SomeCxFD_WOMEN <- as.numeric(lhs["AUD_SomeCxFD_WOMEN"])
-  
+
   data <- data %>%
     mutate(RR_AUD = ifelse(microsim.init.education=="LEHS" & microsim.init.sex=="m",
                                 exp(B_AUD_LEHS_MEN + B_AUD_GPD_MEN*microsim.init.alc.gpd + B_AUD_LEHSxGPD_MEN*microsim.init.alc.gpd),
@@ -49,17 +51,20 @@ AUDInteraction <- function(data,lhs){
                                                           exp(B_AUD_SomeC_WOMEN + B_AUD_GPD_WOMEN*microsim.init.alc.gpd + B_AUD_SomeCxGPD_WOMEN*microsim.init.alc.gpd),
                                                           ifelse(microsim.init.education=="College" & microsim.init.sex=="f",
                                                                  exp(B_AUD_GPD_WOMEN*microsim.init.alc.gpd), NA)))))),
-           RR_AUD = ifelse(formerdrinker==1 & microsim.init.education=="LEHS" & microsim.init.sex=="m", 
+           RR_AUD = ifelse(formerdrinker==1 & microsim.init.education=="LEHS" & microsim.init.sex=="m",
                                  exp(AUD_FD_LEHS_MEN + AUD_FD_MEN*microsim.init.alc.gpd + AUD_LEHSxFD_MEN*microsim.init.alc.gpd),
                                  ifelse(formerdrinker==1 & microsim.init.education=="SomeC" & microsim.init.sex=="m",
                                         exp(AUD_FD_SomeC_MEN + AUD_FD_MEN*microsim.init.alc.gpd + AUD_SomeCxFD_MEN*microsim.init.alc.gpd),
                                         ifelse(formerdrinker==1 & microsim.init.education=="College" & microsim.init.sex=="m",
                                                exp(AUD_FD_MEN*microsim.init.alc.gpd),
                                                ifelse(formerdrinker==1 & microsim.init.education=="LEHS" & microsim.init.sex=="f",
-                                                      exp(AUD_FD_LEHS_WOMEN + AUD_FD_WOMEN*microsim.init.alc.gpd + AUD_LEHSxFD_WOMEN*microsim.init.alc.gpd), 
+                                                      exp(AUD_FD_LEHS_WOMEN + AUD_FD_WOMEN*microsim.init.alc.gpd + AUD_LEHSxFD_WOMEN*microsim.init.alc.gpd),
                                                       ifelse(formerdrinker==1 & microsim.init.education=="SomeC" & microsim.init.sex=="f",
                                                             exp(AUD_FD_SomeC_WOMEN + AUD_FD_WOMEN*microsim.init.alc.gpd + AUD_SomeCxFD_WOMEN*microsim.init.alc.gpd),
                                                             ifelse(formerdrinker==1 & microsim.init.education=="College" & microsim.init.sex=="f",
                                                                    exp(AUD_FD_WOMEN*microsim.init.alc.gpd), RR_AUD)))))))
   return(data)
 }
+# test for exploring the RR dose response curve
+test <- data %>% dplyr::select(microsim.init.sex, microsim.init.education, microsim.init.alc.gpd, RR_AUD) %>% distinct()
+ggplot(test, aes(x=microsim.init.alc.gpd, y=RR_AUD)) + geom_point() + facet_grid(cols=vars(microsim.init.education), rows=vars(microsim.init.sex), scales="free")
