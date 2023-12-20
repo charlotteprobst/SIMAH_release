d1c3d4721ccd8f55a23c31a46a55209ad6e3dc59
Laura Llamosas-Falcón <llamosasfalcon@gmail.com>
2023-12-20 11:34:08 -0500

    Update 1_run_microsimulation_testing.R

--- microsim/2_run_microsimulation/1_run_microsimulation_testing.R
+++ microsim/2_run_microsimulation/1_run_microsimulation_testing.R
@@ -51,6 +51,7 @@ minyear <- 2000
 maxyear <- 2003
 
 Output <- list()
+updatingeducation <- 0
 Output <- run_microsim_alt(seed=1,samplenum=1,basepop,brfss,
                            death_counts,
                            updatingeducation, education_transitions,
@@ -61,7 +62,7 @@ Output <- run_microsim_alt(seed=1,samplenum=1,basepop,brfss,
                            policy=0, percentreduction=0.1, year_policy, inflation_factors,
                            age_inflated,
                            update_base_rate,
-                           minyear=2000, maxyear=2003, output="demographics")
+                           minyear=2000, maxyear=2003, output="mortality")
 
 alcohol_type <- "categorical"
 
