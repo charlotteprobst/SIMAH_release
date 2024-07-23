b5aa115937fba4f74e25d75100520fe1f6be4065
PeteLord9 <psjlord1@sheffield.ac.uk>
2024-07-23 09:27:39 +0100

    Peter - adding working directory

--- microsim/2_run_microsimulation/1_run_microsimulation_testing.R
+++ microsim/2_run_microsimulation/1_run_microsimulation_testing.R
@@ -14,39 +14,42 @@ library(lhs)
 library(truncnorm)
 library(data.table)
 library(gridExtra)
+library(rlang)
 options(dplyr.summarise.inform = FALSE)
 
-###set working directory to the main "SIMAH" folder in your directory 
+###set working directory to the main "SIMAH" folder in your directory
 # WorkingDirectory <- "U:/SIMAH/"
 # WorkingDirectory <- "C:/Users/laura/Documents/CAMH/SIMAH/"
-WorkingDirectory <- "~/Google Drive/SIMAH Sheffield/"
+# WorkingDirectory <- "~/Google Drive/SIMAH Sheffield/"
 # WorkingDirectory <- "C:/Users/marie/Dropbox/NIH2020/"
 # WorkingDirectory <- "C:/Users/cmp21seb/Documents/SIMAH/"
+WorkingDirectory <- "C:/Users/peter/Documents/GitHub/"
+
 
 DataDirectory <- paste0(WorkingDirectory, "SIMAH_workplace/microsim/1_input_data/")
 
 # load in microsim R package
 setwd(paste(WorkingDirectory))
 
-install("SIMAH_code/microsimpackage", dep=T)
+# install("SIMAH_code/microsimpackage", dep=T)
 
 source("SIMAH_code/microsim/2_run_microsimulation/0_model_settings.R")
 
 # alcohol_transitions <- read.csv("SIMAH_workplace/microsim/1_input_data/alcohol_transitions_new.csv")
 alcohol_transitions <- readRDS(paste0(DataDirectory, "final_alc_transitionsUSA.RDS"))
 
-output_type <- "mortality"
+output_type <- "demographics"
 
-# random number seed - sample random number 
+# random number seed - sample random number
 seed <- as.numeric(sample(1:100, 1))
 
-# sample number - set to 1 when just running 1 simulation 
+# sample number - set to 1 when just running 1 simulation
 samplenum <- 1
 
-# set lhs to the first element of the lhs list- for testing 
+# set lhs to the first element of the lhs list- for testing
 lhs <- lhs[[1]]
 
-# set minyear and maxyear 
+# set minyear and maxyear
 minyear <- 2000
 maxyear <- 2005
 updatingeducation <- 0
@@ -61,28 +64,30 @@ Output <- run_microsim_alt(seed=1,samplenum=1,basepop,brfss,
                            policy=0, percentreduction=0.1, year_policy, inflation_factors,
                            age_inflated,
                            update_base_rate,
-                           minyear=2000, maxyear=2003, output="mortality")
+                           minyear=2000, maxyear=2005, output_type)
 
 alcohol_type <- "categorical"
 
 # Output <- readRDS("SIMAH_workplace/microsim/2_output_data/output_baserate_multiple.RDS")
 
-if(output_type=="demographics"){
+
+if(output_type=="demographics"){ # this doesn't run for output_type == mortality
   summary <- summarise_education_output(Output, SelectedState, DataDirectory)
 }else if(output_type=="alcohol"){
   if(alcohol_type=="categorical"){
-    summary <- summarise_alcohol_output(Output, SelectedState, DataDirectory)
-  }else if(alcohol_type=="continuous"){
+    summary <- summarise_alcohol_output(Output[[1]], SelectedState, DataDirectory)
+}else if(alcohol_type=="continuous"){
     summary <- summarise_alcohol_output_continuous(Output[[2]], SelectedState, DataDirectory)
   }
 }else if(output_type=="mortality"){
-  summary1 <- summarise_mortality_output(Output[[1]], SelectedState, DataDirectory, diseases, 2010)
-  summary2 <- summarise_mortality_output(Output2, SelectedState, DataDirectory, diseases, 2000)
-}
+  summary <- summarise_mortality_output(Output,SelectedState, DataDirectory, diseases, 2010)
+} # legacy - can be customised for each user
+
 # data frame containing mortality outputs
 summary_mortality <- summary[[1]]
-# plots for mortality 
-summary[[2]]
+# plots for mortality
+summary[[2]] # Men
+summary[[3]] # Women
 
 # save a copy of the plot
 ggsave("SIMAH_workplace/microsim/2_output_data/mortality_summary_multiple_calibration_best.png", plot, dpi=300,
