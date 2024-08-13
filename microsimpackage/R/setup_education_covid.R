0337e6594835358280636168d7c62cc7af4b4fb5
charlottembuckley <c.m.buckley@sheffield.ac.uk>
2024-08-13 11:35:57 +0100

    fixing naming conventions in setup education covid function

--- microsimpackage/R/setup_education_covid.R
+++ microsimpackage/R/setup_education_covid.R
@@ -7,22 +7,22 @@
 #' @examples
 #' setup_education
 setup_education_covid <- function(basepop,y){
-  basepop$agecat <- ifelse(basepop$microsim.init.age==18, "18",
-                           ifelse(basepop$microsim.init.age==19, "19",
-                                  ifelse(basepop$microsim.init.age==20, "20",
-                                                ifelse(basepop$microsim.init.age>=21 & basepop$microsim.init.age<=25, "21-25","26+"))))
-  basepop$state <- ifelse(basepop$microsimnewED=="LEHS", 1,
-                          ifelse(basepop$microsimnewED=="SomeC1",2,
-                                 ifelse(basepop$microsimnewED=="SomeC2",3,
-                                        ifelse(basepop$microsimnewED=="SomeC3",4,
-                                               ifelse(basepop$microsimnewED=="College",5, NA)))))
-  basepop$year <- ifelse(y<=2019, "1999-2019", 
+  basepop$agecat <- ifelse(basepop$age==18, "18",
+                           ifelse(basepop$age==19, "19",
+                                  ifelse(basepop$age==20, "20",
+                                                ifelse(basepop$age>=21 & basepop$age<=25, "21-25","26+"))))
+  basepop$state <- ifelse(basepop$education_detailed=="LEHS", 1,
+                          ifelse(basepop$education_detailed=="SomeC1",2,
+                                 ifelse(basepop$education_detailed=="SomeC2",3,
+                                        ifelse(basepop$education_detailed=="SomeC3",4,
+                                               ifelse(basepop$education_detailed=="College",5, NA)))))
+  basepop$year <- ifelse(y<=2019, "1999-2019",
                          ifelse(y==2020|y==2021|y==2022, "2019-2022", NA))
-  basepop$racecat <- ifelse(basepop$microsim.init.race=="BLA", "black",
-                            ifelse(basepop$microsim.init.race=="WHI","white",
-                                   ifelse(basepop$microsim.init.race=="OTH", "other",
-                                          ifelse(basepop$microsim.init.race=="SPA","hispanic",NA))))
-  basepop$cat <- paste(basepop$agecat, basepop$microsim.init.sex,
+  basepop$racecat <- ifelse(basepop$race=="Black", "black",
+                            ifelse(basepop$race=="White","white",
+                                   ifelse(basepop$race=="Others", "other",
+                                          ifelse(basepop$race=="Hispanic","hispanic",NA))))
+  basepop$cat <- paste(basepop$agecat, basepop$sex,
                        basepop$racecat,
                        "STATEFROM", basepop$state, sep="_")
   basepop$racecat <- NULL
