cc471932ed5efa2610c3b069d3de8dc67addb5b0
caroki <carolin.kilian@mailbox.org>
2024-09-15 13:10:30 +0200

    Updated figure including individual-level BRFSS data

--- lancet_commentary/Fig_GPDxLVDC.R
+++ lancet_commentary/Fig_GPDxLVDC.R
@@ -18,8 +18,7 @@ rm(list = ls())
 setwd("/Users/carolinkilian/Desktop/SIMAH_workplace/")
 
 # Load files
-brfss.raw <- readRDS("lancet_commentary/20230925_brfss_clean.RDS") %>% 
-  filter(gramsperday > 0) %>% mutate(YEAR = is.numeric(YEAR))
+brfss <- read.csv("lancet_commentary/brfss_0022_summary.csv")
 mort.raw <- read.csv("lancet_commentary/alvdcmort_0022_SIMAH.csv")
 pop.raw <- read.csv("lancet_commentary/ACS_popcounts_2000_2021_updated.csv") 
 
@@ -27,20 +26,11 @@ pop.raw <- read.csv("lancet_commentary/ACS_popcounts_2000_2021_updated.csv")
 ## Prepare data 
 ## ----------------------------------------------------------------
 
-# get mean drinking per year, sex and education group 
-
-# set survey design
-options(survey.lonely.psu = "adjust")
-brfss <- svydesign(ids = ~X_PSU, strata = ~interaction(X_STSTR, YEAR), 
-                   weights = ~final_sample_weight_adj, nest = T, data = brfss.raw)
-
-gpd <- svyby(~gramsperday, ~sex_recode + education_summary + YEAR, subset(brfss, gramsperday > 0), svymean, multicore=TRUE) %>% 
-  select(c("sex_recode", "education_summary", "YEAR", "gramsperday")) %>% 
-  mutate(gpd_lci = gpd - (1.96*gpd_se),
-         gpd_uci = gpd + (1.96*gpd_se)) %>%
-  mutate(sex = ifelse(sex_recode == "Female", "Women", ifelse(sex_recode == "Male", "Men", NA))) %>% 
-  rename("year" = "YEAR", "edclass" = "education_summary") %>%
-  dplyr::select(c(year, sex, edclass, gpd, gpd_lci, gpd_uci))
+#brfss.raw <- readRDS("lancet_commentary/brfss_full_2000_2022.RDS") %>% 
+#  do.call(rbind,.) %>% 
+#  select(YEAR, final_sample_weight, X_STSTR, X_PSU,
+#         sex_recode, education_summary, gramsperday)
+#write.csv(brfss.raw, "lancet_commentary/selected_brfss_full_2000_2022.csv", na = ".")
 
 # prepare population data
 
@@ -83,10 +73,13 @@ mort <- mort.raw %>%
 ## Visualize
 ## ----------------------------------------------------------------
 
-pdat <- left_join(gpd, mort) %>%
+pdat <- left_join(brfss, mort) %>%
   filter(year <= 2021) %>% 
-  mutate(edclass = factor(edclass, levels = c("LEHS", "SomeC", "College"),
+  mutate(alccat3 = alccat3*100, alccat3_lci = alccat3_lci*100, alccat3_uci = alccat3_uci*100,
+         edclass = factor(edclass, levels = c("LEHS", "SomeC", "College"),
                           labels = c("Less than high school", "Some college", "Bachelor's degree or higher")))
+
+# GPD and ALVDC
 
 scaleFactor <- max(pdat$ALVDCasrate) / max(pdat$gpd)
 
@@ -100,14 +93,32 @@ theme_barplot <- theme_bw() +
         legend.position = "none")
 
 ggplot(pdat, aes(x = year, group = sex)) +
-  geom_line(aes(y = gpd*scaleFactor, linetype = sex), color = "#782170", linewidth = 1) + 
+  geom_line(aes(y = gpd*scaleFactor, linetype = sex), color = "#729928", linewidth = 1) + 
   geom_ribbon(aes(ymin = gpd_lci*scaleFactor, ymax = gpd_uci*scaleFactor,
-                  linetype = sex), fill = "#782170", alpha = 0.1) + 
+                  linetype = sex), fill = "#729928", alpha = 0.1) + 
   geom_line(aes(y = ALVDCasrate, linetype = sex), color = "#4568BA", linewidth = 1) + 
   scale_y_continuous(name = "Age-standardized mortality rate (per 100,000)",
                      sec.axis = sec_axis(~./scaleFactor, name = "Average grams of pure alcohol per day")) + 
   facet_grid(cols = vars(edclass)) + 
-  ggtitle("Alcohol consumption and alcohol-related liver disease in the US (2000-2021)") +
+  ggtitle("Alcohol consumption and alcohol-related liver cirrhosis in the US (2000-2021)") +
   theme_barplot
 
-ggsave(paste0("lancet_commentary/Fig1_ALVDCxGPD_", Sys.Date(), ".jpg"), dpi=300, width = 10, height = 6)
+ggsave(paste0("lancet_commentary/Fig1_ALVDCxGPD_", Sys.Date(), ".jpg"), dpi=300, width = 10, height = 5)
+
+# ALCCAT3 and ALVDC
+
+scaleFactor <- max(pdat$ALVDCasrate) / max(pdat$alccat3)
+
+ggplot(pdat, aes(x = year, group = sex)) +
+  geom_bar(aes(y = alccat3*scaleFactor), stat = "identity", fill = "#729928", alpha = 0.5) + 
+  geom_linerange(aes(ymin = alccat3_lci*scaleFactor, ymax = alccat3_uci*scaleFactor), 
+                 stat = "identity", alpha = 0.3) + 
+  geom_line(aes(y = ALVDCasrate), color = "#4568BA", linewidth = 1) + 
+  scale_y_continuous(name = "Age-standardized mortality rate (per 100,000)",
+                     sec.axis = sec_axis(~./scaleFactor, name = "Prevalence of high-risk alcohol use (%)"),
+                     limits = c(0, 30)) + 
+  facet_grid(cols = vars(edclass), rows = vars(sex)) + 
+  ggtitle("High-risk alcohol use and alcohol-related liver cirrhosis in the US (2000-2021)") +
+  theme_barplot
+
+ggsave(paste0("lancet_commentary/Fig1_ALVDCxAlcCat3_", Sys.Date(), ".jpg"), dpi=300, width = 10, height = 6)
