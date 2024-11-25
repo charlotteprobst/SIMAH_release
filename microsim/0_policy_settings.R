c623105ba81530c5789ec02cb56d8b0d986d536a
caroki <carolin.kilian@mailbox.org>
2024-11-25 08:52:11 +0100

    Reorganised policy settings
    * define policy scenarios in this script
    * include uncertainty count parameter

--- microsim/0_policy_settings.R
+++ microsim/0_policy_settings.R
@@ -11,12 +11,22 @@ options(scipen=999)
 
 # general policy settings
 policy <- 1
-year_policy <- 2015
+year_policy <- 2019
 policy_int <- "price"
-#scenarios <- c(0.05, 0.1, 0.2)
+n_uncertainty <- 10
+
+# define policy modelling scenarios 
+# for price policites specific to beer, wine, spirits
+scenarios <- cbind(policymodel = 1:4,
+                   scenario = c("0.1,0.1,0.1",
+                                "0.3,0.3,0.3",
+                                "0.3,0.1,0.3",
+                                "0.5,0.1,0.5")) %>% as.data.frame()
 
 ######################DO NOT EDIT BELOW HERE ##################################################
 
 policy_setting <- read.csv(paste0(DataDirectory,"input_policy_sim.csv")) %>% 
-  filter(policy %in% policy_int)
+  filter(policy %in% policy_int) %>% 
+  mutate(beverage = as.factor(beverage))
+
 
