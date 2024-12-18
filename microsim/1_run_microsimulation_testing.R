0abf77d54ff0bcec29b0c1b64de8d2c048795f0a
caroki <carolin.kilian@mailbox.org>
2024-12-18 14:23:52 +0100

    Updated to work with new sampleseeds function

--- microsim/1_run_microsimulation_testing.R
+++ microsim/1_run_microsimulation_testing.R
@@ -17,7 +17,8 @@ library(data.table)
 library(gridExtra)
 library(doParallel)
 options(dplyr.summarise.inform = FALSE)
-registerDoParallel(1)
+cl <- makeCluster(10)
+registerDoParallel(cl)
 
 library(beepr)
 
@@ -48,77 +49,36 @@ source("SIMAH_code/microsim/0_policy_settings.R")
 # load microsim files
 source("SIMAH_code/microsim/0_load_microsim_files.R")
 
-# set up the number of samples to be run
-nsamples <- 1 # indicates samples per same sample seed (for calibration purposes only)
-nreps <- 1 # indicates number of different sample seeds
-n_uncertainty <- 100
-
-# generate list of samples to be run with random number seeds
-sampleseeds <- expand.grid(samplenum = 1:nsamples, seed=1:nreps)
-sampleseeds$seed <- sample(1:3000, nrow(sampleseeds), replace=F)
-
 # read in calibrated education transitions
 education_transitionsList <- read_rds(paste0(WorkingDirectory, "SIMAH_workplace/microsim/2_output_data/education_calibration", "/transitionsList-10",".RDS"))
 for(i in 1:length(education_transitionsList)){
   education_transitionsList[[i]]$cat <- gsub("1999-2019+_","",education_transitionsList[[i]]$cat)
 }
 
-# add the final education model to be run to the sampleseeds file 
-edmodels <- list()
-for(i in 1:ceiling(nrow(sampleseeds)/length(education_transitionsList))){
-  edmodels[[paste(i)]] <- data.frame(education_model = sample(1:length(education_transitionsList), replace=F))
-}
-edmodels <- edmodels %>% bind_rows()
-
-sampleseeds$educationmodel <- edmodels$education_model[1:nrow(sampleseeds)]
-
 # read in calibrated alcohol transitions 
 alcohol_transitions <- read_csv(paste0(WorkingDirectory, "SIMAH_workplace/microsim/2_output_data/alcohol_calibration/lhs_regression-4.csv"), show_col_types = FALSE)
 alcohol_transitionsList <- list()
 for(i in 1:max(alcohol_transitions$sample)){
-  alcohol_transitionsList[[i]] <- alcohol_transitions %>% filter(sample==i)}
-
-# add the final alcohol model to be run to the sampleseeds file 
-alcmodels <- list()
-for(i in 1:ceiling(nrow(sampleseeds)/length(alcohol_transitions))){
-  alcmodels[[paste(i)]] <- data.frame(alcohol_model = sample(1:length(alcohol_transitions), replace=F))
+  alcohol_transitionsList[[i]] <- alcohol_transitions %>% filter(sample==i)
 }
-alcmodels <- alcmodels %>% bind_rows()
 
-sampleseeds$alcoholmodel <- alcmodels$alcohol_model[1:nrow(sampleseeds)]
-
-# set up scenarios and policy settings
-sampleseeds <- sampleseeds %>% expand(sampleseeds, policy_setting, scenarios)
-
-# sample policy parameters here based on sampleseeds groups by samplenum, seed, edu/alcmodel
-sampleseeds <- sample_policy_parameters(sampleseeds, n_uncertainty)
-
-# get counterfactual scenario
-counterfactual <- sampleseeds %>% 
-  group_by(samplenum, seed, educationmodel, alcoholmodel) %>% 
-  slice(1) %>% mutate(policymodel = 0, scenario = "0,0,0", setting = "counterfactual")
-sampleseeds <- rbind(sampleseeds, counterfactual)
+# read in sampleseeds file (or source 0_generate_sampleseeds.R)
+sampleseeds <- read.csv("SIMAH_workplace/microsim/2_output_data/sampleseeds/output-policy_sampleseeds_100NReps2024-12-16.csv")
+# source("SIMAH_code/microsim/0_generate_sampleseeds.R")
 
 # FOR NOW: limit to standard model and only one policy model
 sampleseeds <- sampleseeds %>% filter(setting == "standard") %>%
-  filter(policymodel == 0 | policymodel == 4)
-
-# pick a random education / alcohol model to use (for testing purposes)
-# this picks a random model from the calibrated education / alcohol models
-# samplenum <- sample(1:300, 1, replace=F)
-
-# education_transitions <- education_transitions[[samplenum]]
-# alcohol_transitions <- alcohol_transitions %>% filter(sample==samplenum)
+  filter(policymodel == 5)
 
 # read in the categorical to continuous distributions
 catcontmodel <- read.csv("SIMAH_workplace/microsim/2_output_data/alcohol_calibration/calibration_continuous_distribution.csv")
 
-output_type <- "percentreduction"
+output_type <- "alcoholcont"
 
 # set minyear and maxyear 
 minyear <- 2000
-maxyear <- 2001
-year_policy <- 2001
+maxyear <- 2019
+year_policy <- 2019
 
 diseases <- NULL
 
@@ -126,7 +86,7 @@ diseases <- NULL
 
 Output <- list()
 baseorig <- basepop
-Output <- foreach(i=1:nrow(sampleseeds), .inorder=TRUE) %do% {
+Output <- foreach(i=1:nrow(sampleseeds), .inorder=TRUE) %dopar% {
   print(i)
   # set seed and sample number for current iteration
   samplenum <- as.numeric(sampleseeds$samplenum[i])
@@ -168,12 +128,10 @@ Output <- foreach(i=1:nrow(sampleseeds), .inorder=TRUE) %do% {
 }
 beep()
 
-Sys.Date <- "2024-11-14"
-Output <- do.call(rbind,Output) %>% 
-  group_by(nunc) %>% sample_frac(0.001)
+Sys.Date <- "2024-11-16"
+Output <- do.call(rbind,Output) 
 # save the output in the output directory
-write.csv(Output, paste0(OutputDirectory, "/output-policy_alcoholcontcat_", Sys.Date, ".csv"), row.names=F)
-write.csv(sampleseeds, paste0(OutputDirectory, "/output-policy_sampleseeds_", Sys.Date, ".csv"), row.names=F)
+write.csv(Output, paste0(OutputDirectory, "/output-policy_alcoholcontcat_NRep100_", Sys.Date, ".csv"), row.names=F)
 
 plot <- summarise_alcohol_policy(Output, SelectedState = "USA", version = "standard")
 write.csv(plot[[1]], paste0(OutputDirectory, "/tab-policy_alccat_", Sys.Date, ".csv"))
