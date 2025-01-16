b22d1572470100420552754da061fb265c6f96ef
caroki <carolin.kilian@mailbox.org>
2025-01-16 10:43:14 +0100

    Generate output summary for paper
    * Generates Output for Pricing Policy Paper
    * R Scripts provide Figures and Tables for Paper
    * Requires data input from microsimulation

---
+++ pricing_policy/Summary_PricePolicyPaper_FigMain.R
@@ -0,0 +1,259 @@
+#' Summarise mean alcohol consumption for price policy paper (Kilian et al.)
+#'
+#' @param
+#' @keywords alcohol postprocessing
+#' @export
+#' @examples
+
+# libraries
+library(dplyr)
+library(tidyr)
+library(data.table)
+library(ggplot2)  
+library(ggpubr)
+
+WorkingDirectory <- "/Users/carolinkilian/Desktop/SIMAH_workplace/microsim/2_output_data/"
+OutputDirectory <- "/Users/carolinkilian/Desktop/SIMAH_workplace/pricing_policy/"
+ 
+# load data
+data_alccont <- read.csv(paste0(WorkingDirectory, "2025-01-13/output-policy_alcoholcont_2025-01-13.csv"))
+data_alccat <- read.csv(paste0(WorkingDirectory, "2025-01-13/output-policy_alcoholcat_2025-01-13.csv"))
+data_alccontcat <- read.csv(paste0(WorkingDirectory, "2025-01-13/output-policy_alcoholcontcat_2025-01-13.csv"))
+  
+# set ggplot layout
+options(digits = 4)
+  
+col_green <- colorRampPalette(c("#74C67A", "#137751"))
+col_blue <- colorRampPalette(c("#82A9B2", "#2D6D7A"))
+col_red <- colorRampPalette(c("#FAAA74", "#9F4238"))
+cb2 <- col_blue(2)
+cr2 <- col_red(2)
+c4 <- c(cb2, cr2)
+cg3 <- col_green(3)
+
+ggtheme <- theme_bw() + theme(legend.position = "right",
+                              strip.background = element_rect(fill = "white"),
+                              panel.spacing = unit(1, "lines"),
+                              text = element_text(size = 16, color = "black"),
+                              strip.text = element_text(size = 16, color = "black"),
+                              axis.text = element_text(size = 14, color = "black"))
+
+# --------------------------------------------
+# MAIN RESULTS
+# --------------------------------------------
+  
+# Figure 1) Main alcohol consumption levels (continuous alcohol outcome)
+  
+alccont <- data_alccont %>%
+  filter(setting == "standard") %>% 
+  # set labels
+  mutate(year = as.numeric(as.character(year)),
+         sex = ifelse(sex=="m","Men","Women"),
+         education = factor(education, 
+                            levels = c("LEHS", "SomeC", "College"),
+                            labels = c("High school or less", "Some college", "College +")),
+         scenario = factor(case_when(
+           policymodel == 1 ~ "Reference",
+           policymodel != 1  ~ paste0("Scenario ", policymodel-1),
+           TRUE ~ NA), levels = c("Reference", 
+                                  "Scenario 1", "Scenario 2", 
+                                  "Scenario 3", "Scenario 4"))) %>%
+  # average across random number seeds
+  group_by(scenario, nunc, year, sex, education) %>% 
+  summarise(meansimulation = mean(as.numeric(meansimulation)),
+            sesimulation = mean(as.numeric(sesimulation)))
+  
+# get mean across multiple model runs 
+mean <- alccont %>%
+  group_by(scenario, year, sex, education) %>% 
+  summarise(meangpd = mean(as.numeric(meansimulation)),
+            segpd = mean(as.numeric(sesimulation)))
+    
+# get uncertainty across multiple model runs (min/max)
+var <- alccont %>% filter(year != "2000") %>%
+  group_by(scenario, year, sex, education) %>% 
+  mutate(min = min(as.numeric(meansimulation)),
+         max = max(as.numeric(meansimulation)),
+         var = ifelse(meansimulation == min, "min",
+                      ifelse(meansimulation == max, "max", NA))) %>% ungroup() %>%
+  filter(meansimulation == min | meansimulation == max) %>%
+  dplyr::select(c(scenario, nunc, year, sex, education, var)) 
+    
+output1 <- merge(alccont, var, all.y = T) %>% 
+  rename("meangpd" = "meansimulation", "segpd" = "sesimulation") %>% 
+  dplyr::select(c(scenario, year, sex, education, meangpd, segpd, var)) %>%
+  pivot_wider(names_from = "var", values_from = c("meangpd", "segpd")) %>%
+  left_join(mean, .)
+    
+ggplot() + 
+  geom_vline(xintercept = 2019, linetype = "dashed", linewidth = 0.5, colour = "grey") + 
+  geom_line(data = output1[output1$scenario == "Reference" & output1$year != 2019,], 
+            aes(y = meangpd_min, x = year, colour = scenario), linewidth = 0.5, alpha = 0.5) + 
+  geom_line(data = output1[output1$scenario == "Reference" & output1$year != 2019,], 
+            aes(y = meangpd_max, x = year, colour = scenario), linewidth = 0.5, alpha = 0.5) + 
+  geom_line(data = output1[output1$scenario == "Reference" & output1$year != 2019,], 
+            aes(y = meangpd, x = year, colour = scenario), linewidth = 0.5) +
+  geom_point(data = output1[output1$year == 2019,], 
+             aes(y = meangpd, x = year, colour = scenario, fill = scenario), shape = 23, size = 2) +
+  geom_errorbar(data = output1[output1$year == 2019,], 
+                aes(ymin = meangpd_min, ymax = meangpd_max, x = year, colour = scenario), width = 0.2) + 
+  facet_grid(cols = vars(education), rows = vars(sex), scales = "free") + 
+  ylim(0,NA) + ylab("Mean grams of pure alcohol per day") +
+  scale_x_continuous(breaks = seq(2000, 2019, 3), limits = c(2000, 2019+0.1)) + 
+  scale_color_manual(values = c5, name = "") + scale_fill_manual(values = c5, name = "") + 
+  ggtheme + xlab("") 
+
+ggsave(paste0(OutputDirectory, Sys.Date(), "/Fig1_MeanGPD_", Sys.Date(), ".png"), height = 7, width = 14, dpi = 300)
+
+# Figure 2) Change in the prevalence of non-drinkers (categorial alcohol outcome)
+
+# generate output for categorical alcohol use
+
+alccat <- data_alccat %>%
+  filter(setting == "standard") %>% 
+  # set labels
+  mutate(year = as.numeric(as.character(year)),
+         sex = ifelse(sex=="m","Men","Women"),
+         education = factor(education, 
+                            levels = c("LEHS", "SomeC", "College"),
+                            labels = c("High school or less", "Some college", "College +")),
+         scenario = factor(case_when(
+           policymodel == 1 ~ "Reference",
+           policymodel != 1  ~ paste0("Scenario ", policymodel-1),
+           TRUE ~ NA), levels = c("Reference", 
+                                  "Scenario 1", "Scenario 2", 
+                                  "Scenario 3", "Scenario 4")),
+         alc_cat = factor(alc_cat, 
+                          levels = c("High risk", "Medium risk", "Low risk", "Non-drinker"),
+                          labels = c("Category III", "Category II", "Category I", "Abstinence"))) %>%
+  # average across random number seeds
+  group_by(scenario, nunc, year, sex, education, alc_cat) %>% 
+  summarise(propsimulation = mean(as.numeric(propsimulation)))
+
+# get mean across multiple model runs 
+mean <- alccat %>%
+  group_by(scenario, year, sex, education, alc_cat) %>% 
+  summarise(meanprop = mean(as.numeric(propsimulation)))
+    
+# get uncertainty across multiple model runs (min/max)
+var <- alccat %>% filter(year != "2000") %>%
+  group_by(scenario, year, sex, education, alc_cat) %>% 
+  mutate(min = min(as.numeric(propsimulation)),
+         max = max(as.numeric(propsimulation)),
+         var = ifelse(propsimulation == min, "min",
+                      ifelse(propsimulation == max, "max", NA))) %>% ungroup() %>%
+  filter(propsimulation == min | propsimulation == max) %>%
+  dplyr::select(c(scenario, nunc, year, sex, education, alc_cat, var)) 
+
+output2 <- merge(alccat, var, all.y = T) %>% 
+  rename("meanprop" = "propsimulation") %>% 
+  dplyr::select(c(scenario, year, sex, education, alc_cat, meanprop, var)) %>%
+  pivot_wider(names_from = "var", values_from = "meanprop") %>%
+  left_join(mean, .) %>% 
+  # select just non-drinkers and year of policy
+  filter(year == 2019 & alc_cat == "Abstinence") 
+
+# re-arrange to show value of reference plus scenarios
+reference <- output2 %>% filter(scenario == "Reference") %>% 
+  mutate(condition = "reference") %>% 
+  group_by(year, sex, education, alc_cat, meanprop, min, max, condition) %>% 
+  tidyr::expand(scenario = 1:4) %>% 
+  mutate(scenario = paste0("Scenario ", scenario))
+
+output2 <- output2 %>% filter(scenario != "Reference") %>% 
+  mutate(condition = "model") %>% 
+  rbind(., reference) %>% 
+  mutate(condition = factor(condition, levels = c("model", "reference"))) 
+      
+ggplot() + 
+  geom_bar(data = output2[output2$condition == "model",], 
+           aes(y = meanprop, x = scenario, color = scenario, fill = scenario), 
+           stat = "identity", linetype = "dashed", alpha = 0.5) +
+  geom_bar(data = output2[output2$condition == "reference",], 
+           aes(y = meanprop, x = scenario), 
+           stat = "identity", color = "black", fill = "black", alpha = 0.5) +
+  geom_errorbar(data = output2[output2$condition == "model",], 
+                aes(ymin = min, ymax = max, x = scenario, colour = scenario), width = 0.2) +
+  facet_grid(cols = vars(education), rows = vars(sex), scales = "free") + 
+  scale_y_continuous(labels = scales::percent, limits = c(0,NA)) + ylab("Prevalence of abstinence (%)") +
+  scale_color_manual(values = c4, name = "") + scale_fill_manual(values = c4, name = "") + 
+  scale_linetype_manual(values = c("dashed", "solid"), name = "") +
+  ggtheme + xlab("") + theme(legend.position = "none")
+
+ggsave(paste0(OutputDirectory, Sys.Date(), "/Fig2_PrevNonDrinkers_", Sys.Date(), ".png"), height = 7, width = 14, dpi = 300)
+
+# Figure 3) Change in mean consumption by alcohol use category (continuous-categorical alcohol outcome)
+    
+alccontcat <- data_alccontcat %>%
+  filter(!is.na(alc_cat_2018) & alc_cat_2018 != "Non-drinker") %>% 
+  filter(setting == "standard") %>% 
+  # set labels
+  mutate(year = as.numeric(as.character(year)),
+         sex = ifelse(sex=="m","Men","Women"),
+         education = factor(education, 
+                            levels = c("LEHS", "SomeC", "College"),
+                            labels = c("High school or less", "Some college", "College +")),
+         scenario = factor(case_when(
+           policymodel == 1 ~ "Reference",
+           policymodel != 1  ~ paste0("Scenario ", policymodel-1),
+           TRUE ~ NA), levels = c("Reference", 
+                                  "Scenario 1", "Scenario 2", 
+                                  "Scenario 3", "Scenario 4")),
+         alc_cat_2018 = factor(alc_cat_2018, 
+                               levels = c("Low risk", "Medium risk", "High risk"),
+                               labels = c("Category I", "Category II", "Category III"))) %>%
+  # average across random number seeds
+  group_by(scenario, nunc, year, sex, education, alc_cat_2018) %>% 
+  summarise(meansimulation = mean(as.numeric(meansimulation)),
+            sesimulation = mean(as.numeric(sesimulation)))
+  
+# get mean across multiple model runs 
+mean <- alccontcat %>%
+  group_by(scenario, year, sex, education, alc_cat_2018) %>% 
+  summarise(meangpd = mean(as.numeric(meansimulation)),
+            segpd = mean(as.numeric(sesimulation)))
+
+# get uncertainty across multiple model runs (min/max)
+var <- alccontcat %>% 
+  group_by(scenario, year, sex, education, alc_cat_2018) %>% 
+  mutate(min = min(as.numeric(meansimulation)),
+         max = max(as.numeric(meansimulation)),
+         var = ifelse(meansimulation == min, "min",
+                      ifelse(meansimulation == max, "max", NA))) %>% ungroup() %>%
+  filter(meansimulation == min | meansimulation == max) %>%
+  dplyr::select(c(scenario, nunc, year, sex, education, alc_cat_2018, var)) 
+
+output3 <- merge(alccontcat, var, all.y = T) %>% 
+  rename("meangpd" = "meansimulation", "segpd" = "sesimulation") %>% 
+  dplyr::select(c(scenario, year, sex, education, alc_cat_2018, meangpd, segpd, var)) %>%
+  pivot_wider(names_from = "var", values_from = c("meangpd", "segpd")) %>%
+  left_join(mean, .) %>% 
+  # use just 2019
+  filter(year == 2019)
+    
+reference <- output3 %>% filter(scenario == "Reference") %>% ungroup() %>%
+      rename("meangpdref" = "meangpd", "maxref" = "meangpd_max", "minref" = "meangpd_min") %>% 
+      dplyr::select(-c(scenario, segpd, segpd_max, segpd_min))
+    
+output3 <- output3 %>% filter(scenario != "Reference") %>% 
+      left_join(., reference) %>% 
+      mutate(diffgpd = meangpd - meangpdref,
+             diffmax = meangpd_min - minref,
+             diffmin = meangpd_max - maxref,
+             percgpd = (meangpd - meangpdref) / meangpdref,
+             percmax = (meangpd_min - minref) / minref,
+             percmin = (meangpd_max - maxref) / maxref) %>% 
+      dplyr::select(c(scenario, year, sex, education, alc_cat_2018,
+                      diffgpd, diffmin, diffmax,
+                      percgpd, percmin, percmax)) 
+    
+ggplot(data = output3) + 
+  geom_point(aes(y = diffgpd, x = scenario, color = alc_cat_2018, fill = alc_cat_2018), shape = 23, size = 2) +
+  geom_errorbar(aes(ymin = diffmin, ymax = diffmax, x = scenario, colour = alc_cat_2018), width = 0.1) +
+  facet_grid(cols = vars(education), rows = vars(sex), scales = "free") + 
+  ylim(NA,0) + ylab("Change in mean grams per day (reference: no policy)") +
+  scale_color_manual(values = cg3, name = "Alcohol use") + scale_fill_manual(values = cg3, name = "Alcohol use") + 
+  ggtheme + xlab("") 
+
+ggsave(paste0(OutputDirectory, Sys.Date(), "/Fig3_GPDbyAlcCat_", Sys.Date(), ".png"), height = 7, width = 14, dpi = 300)
+
---
+++ pricing_policy/Summary_PricePolicyPaper_Tables.R
@@ -0,0 +1,278 @@
+#' Summarise mean alcohol consumption for price policy paper, Tables (Kilian et al.)
+#'
+#' @param
+#' @keywords alcohol postprocessing
+#' @export
+#' @examples
+
+# libraries
+library(dplyr)
+library(tidyr)
+library(data.table)
+library(openxlsx)
+
+WorkingDirectory <- "/Users/carolinkilian/Desktop/SIMAH_workplace/microsim/2_output_data/"
+OutputDirectory <- "/Users/carolinkilian/Desktop/SIMAH_workplace/pricing_policy/"
+ 
+# load data
+data_alccont <- read.csv(paste0(WorkingDirectory, "2025-01-13/output-policy_alcoholcont_2025-01-13.csv"))
+data_alccat <- read.csv(paste0(WorkingDirectory, "2025-01-13/output-policy_alcoholcat_2025-01-13.csv"))
+data_alccontcat <- read.csv(paste0(WorkingDirectory, "2025-01-13/output-policy_alcoholcontcat_2025-01-13.csv"))
+  
+# set standard format
+options(digits = 8)
+
+# --------------------------------------------
+# TABLES
+# --------------------------------------------
+  
+# Table S1) Average alcohol consumption levels in total population (continuous alcohol outcome)
+  
+alccont <- data_alccont %>%
+  # set labels
+  mutate(year = as.numeric(as.character(year)),
+         sex = ifelse(sex=="m","Men","Women"),
+         education = factor(education, 
+                            levels = c("LEHS", "SomeC", "College"),
+                            labels = c("High school or less", "Some college", "College +")),
+         scenario = factor(case_when(
+           policymodel == 1 ~ "Reference",
+           policymodel != 1  ~ paste0("Scenario ", policymodel-1),
+           TRUE ~ NA), levels = c("Reference", 
+                                  "Scenario 1", "Scenario 2", 
+                                  "Scenario 3", "Scenario 4")),
+         setting = factor(setting,
+                          levels = c("standard", "min", "max"),
+                          labels = c("Standard", "Minimum", "Maximum"))) %>%
+  # average across random number seeds
+  group_by(scenario, setting, nunc, year, sex, education) %>% 
+  summarise(meansimulation = mean(as.numeric(meansimulation)))
+  
+# get mean across multiple model runs 
+mean <- alccont %>%
+  group_by(scenario, setting, year, sex, education) %>% 
+  summarise(meangpd = mean(as.numeric(meansimulation)))
+    
+# get uncertainty across multiple model runs (min/max)
+var <- alccont %>% filter(year != "2000") %>%
+  group_by(scenario, setting, year, sex, education) %>% 
+  mutate(min = min(as.numeric(meansimulation)),
+         max = max(as.numeric(meansimulation)),
+         var = ifelse(meansimulation == min, "min",
+                      ifelse(meansimulation == max, "max", NA))) %>% ungroup() %>%
+  filter(meansimulation == min | meansimulation == max) %>%
+  dplyr::select(c(scenario, setting, nunc, year, sex, education, var)) 
+
+# get mean GPD with uncertainty    
+output1A <- merge(alccont, var, all.y = T) %>% 
+  rename("meangpd" = "meansimulation") %>% 
+  dplyr::select(c(scenario, setting, year, sex, education, meangpd, var)) %>%
+  pivot_wider(names_from = "var", values_from = "meangpd") %>%
+  left_join(mean, .) %>% 
+  # select just 2019 or 2000 in reference scenario
+  filter(year == 2019 | (year == 2000 & scenario == "Reference")) 
+
+# get change in mean GPD with uncertainty
+reference <- output1A %>% filter(scenario == "Reference" & year == 2019) %>% ungroup() %>%
+  rename("meangpdref" = "meangpd", "maxref" = "max", "minref" = "min") %>% 
+  dplyr::select(-c(scenario))
+
+output1B <- output1A %>% filter(scenario != "Reference") %>% 
+  left_join(., reference) %>% 
+  mutate(diffgpd = meangpd - meangpdref,
+         diffmax = min - minref,
+         diffmin = max - maxref,
+         percgpd = (meangpd - meangpdref) / meangpdref,
+         percmax = (min - minref) / minref,
+         percmin = (max - maxref) / maxref) %>% 
+  dplyr::select(c(scenario, setting, year, sex, education,
+                  diffgpd, diffmin, diffmax,
+                  percgpd, percmin, percmax)) 
+ 
+# combine elements into Table 1 and formatting
+output1 <- left_join(output1A, output1B) %>% 
+  mutate(mean = case_when(
+          !is.na(meangpd) & year != 2000 ~ paste0(format(round(meangpd, digits = 2), nsmall = 2), " (", format(round(min, digits = 2), nsmall = 2), ", ", format(round(max, digits = 2), nsmall = 2), ")"),
+          !is.na(meangpd) & year == 2000 ~ paste0(format(round(meangpd, digits = 2), nsmall = 2)),
+          TRUE ~ NA), 
+         diff = case_when(
+           !is.na(diffgpd) & year != 2000 ~ paste0(format(round(diffgpd, digits = 2), nsmall = 2), " (", format(round(diffmin, digits = 2), nsmall = 2), ", ", format(round(diffmax, digits = 2), nsmall = 2), ")"),
+           !is.na(diffgpd) & year == 2000 ~ paste0(format(round(diffgpd, digits = 2), nsmall = 2)),
+           TRUE ~ NA),
+         perc = case_when(
+           !is.na(percgpd) & year != 2000 ~ paste0(format(round(percgpd*100, digits = 2), nsmall = 2), " (", format(round(percmin*100, digits = 2), nsmall = 2), ", ", format(round(percmax*100, digits = 2), nsmall = 2), ")"),
+           !is.na(percgpd) & year == 2000 ~ paste0(format(round(percgpd*100, digits = 2), nsmall = 2)),
+           TRUE ~ NA)) %>% 
+  dplyr::select(c(setting, scenario, year, sex, education, mean, diff, perc)) %>% 
+  arrange(setting)
+  
+
+# Table S2) Prevalence of alcohol use categories (categorical alcohol outcome)
+
+alccat <- data_alccat %>%
+  # set labels
+  mutate(year = as.numeric(as.character(year)),
+         sex = ifelse(sex=="m","Men","Women"),
+         education = factor(education, 
+                            levels = c("LEHS", "SomeC", "College"),
+                            labels = c("High school or less", "Some college", "College +")),
+         scenario = factor(case_when(
+           policymodel == 1 ~ "Reference",
+           policymodel != 1  ~ paste0("Scenario ", policymodel-1),
+           TRUE ~ NA), levels = c("Reference", 
+                                  "Scenario 1", "Scenario 2", 
+                                  "Scenario 3", "Scenario 4")),
+         alc_cat = factor(alc_cat, 
+                          levels = c("Non-drinker", "Low risk", "Medium risk", "High risk"),
+                          labels = c("Abstinence", "Category I", "Category II", "Category III")),
+         setting = factor(setting,
+                          levels = c("standard", "min", "max"),
+                          labels = c("Standard", "Minimum", "Maximum"))) %>%
+  # average across random number seeds
+  group_by(scenario, setting, nunc, year, sex, education, alc_cat) %>% 
+  summarise(propsimulation = mean(as.numeric(propsimulation)))
+
+# get mean across multiple model runs 
+mean <- alccat %>%
+  group_by(scenario, setting, year, sex, education, alc_cat) %>% 
+  summarise(meanprop = mean(as.numeric(propsimulation)))
+    
+# get uncertainty across multiple model runs (min/max)
+var <- alccat %>% filter(year != "2000") %>%
+  group_by(scenario, setting, year, sex, education, alc_cat) %>% 
+  mutate(min = min(as.numeric(propsimulation)),
+         max = max(as.numeric(propsimulation)),
+         var = ifelse(propsimulation == min, "min",
+                      ifelse(propsimulation == max, "max", NA))) %>% ungroup() %>%
+  filter(propsimulation == min | propsimulation == max) %>%
+  dplyr::select(c(scenario, setting, nunc, year, sex, education, alc_cat, var)) 
+
+# get prevalence of alcohol use categories
+output2A <- merge(alccat, var, all.y = T) %>% 
+  rename("meanprop" = "propsimulation") %>% 
+  dplyr::select(c(scenario, setting, year, sex, education, alc_cat, meanprop, var)) %>%
+  pivot_wider(names_from = "var", values_from = "meanprop") %>%
+  left_join(mean, .) %>% 
+  # select just 2019 or 2000 in reference scenario
+  filter(year == 2019 | (year == 2000 & scenario == "Reference"))
+
+# get change in mean GPD with uncertainty
+reference <- output2A %>% filter(scenario == "Reference" & year == 2019) %>% ungroup() %>%
+  rename("meanpropref" = "meanprop", "maxref" = "max", "minref" = "min") %>% 
+  dplyr::select(-c(scenario))
+
+output2B <- output2A %>% filter(scenario != "Reference") %>% 
+  left_join(., reference) %>% 
+  mutate(diffprop = meanprop - meanpropref,
+         diffmax = min - minref,
+         diffmin = max - maxref) %>% 
+  dplyr::select(c(scenario, setting, year, sex, education, alc_cat,
+                  diffprop, diffmin, diffmax)) 
+
+# combine elements into Table 2 and formatting
+output2 <- left_join(output2A, output2B) %>% 
+  mutate(mean = case_when(
+          !is.na(meanprop) & year != 2000 ~ paste0(format(round(meanprop, digits = 2), nsmall = 2), " (", format(round(min, digits = 2), nsmall = 2), ", ", format(round(max, digits = 2), nsmall = 2), ")"),
+          !is.na(meanprop) & year == 2000 ~ paste0(format(round(meanprop, digits = 2), nsmall = 2)),
+          TRUE ~ NA), 
+         diff = case_when(
+           !is.na(diffprop) & year != 2000 ~ paste0(format(round(diffprop, digits = 2), nsmall = 2), " (", format(round(diffmin, digits = 2), nsmall = 2), ", ", format(round(diffmax, digits = 2), nsmall = 2), ")"),
+           !is.na(diffprop) & year == 2000 ~ paste0(format(round(diffprop, digits = 2), nsmall = 2)),
+           TRUE ~ NA)) %>% 
+  dplyr::select(c(setting, scenario, year, sex, education, mean, diff, alc_cat)) %>% 
+  pivot_wider(names_from = alc_cat, values_from = c(mean, diff)) %>% 
+  dplyr::select(c(setting, scenario, year, sex, education, 
+                  mean_Abstinence, diff_Abstinence,
+                  `mean_Category I`, `diff_Category I`,
+                  `mean_Category II`, `diff_Category II`,
+                  `mean_Category III`, `diff_Category III`)) %>% 
+  arrange(setting)
+
+
+# Table S3) Simulated average consumption levels by alcohol user category (continuous-categorical alcohol outcome)
+    
+alccontcat <- data_alccontcat %>%
+  filter(!is.na(alc_cat_2018) & alc_cat_2018 != "Non-drinker") %>% 
+  # set labels
+  mutate(year = as.numeric(as.character(year)),
+         sex = ifelse(sex=="m","Men","Women"),
+         education = factor(education, 
+                            levels = c("LEHS", "SomeC", "College"),
+                            labels = c("High school or less", "Some college", "College +")),
+         scenario = factor(case_when(
+           policymodel == 1 ~ "Reference",
+           policymodel != 1  ~ paste0("Scenario ", policymodel-1),
+           TRUE ~ NA), levels = c("Reference", 
+                                  "Scenario 1", "Scenario 2", 
+                                  "Scenario 3", "Scenario 4")),
+         alc_cat_2018 = factor(alc_cat_2018, 
+                               levels = c("Low risk", "Medium risk", "High risk"),
+                               labels = c("Category I", "Category II", "Category III")),
+         setting = factor(setting,
+                          levels = c("standard", "min", "max"),
+                          labels = c("Standard", "Minimum", "Maximum"))) %>%
+  # average across random number seeds
+  group_by(scenario, setting, nunc, year, sex, education, alc_cat_2018) %>% 
+  summarise(meansimulation = mean(as.numeric(meansimulation)),
+            sesimulation = mean(as.numeric(sesimulation)))
+  
+# get mean across multiple model runs 
+mean <- alccontcat %>%
+  group_by(scenario, setting, year, sex, education, alc_cat_2018) %>% 
+  summarise(meangpd = mean(as.numeric(meansimulation)))
+
+# get uncertainty across multiple model runs (min/max)
+var <- alccontcat %>% 
+  group_by(scenario, setting, year, sex, education, alc_cat_2018) %>% 
+  mutate(min = min(as.numeric(meansimulation)),
+         max = max(as.numeric(meansimulation)),
+         var = ifelse(meansimulation == min, "min",
+                      ifelse(meansimulation == max, "max", NA))) %>% ungroup() %>%
+  filter(meansimulation == min | meansimulation == max) %>%
+  dplyr::select(c(scenario, setting, nunc, year, sex, education, alc_cat_2018, var)) 
+
+# get mean GPD by alcohol use categories
+output3A <- merge(alccontcat, var, all.y = T) %>% 
+  rename("meangpd" = "meansimulation") %>% 
+  dplyr::select(c(scenario, setting, year, sex, education, alc_cat_2018, meangpd, var)) %>%
+  pivot_wider(names_from = "var", values_from = "meangpd") %>%
+  left_join(mean, .) %>% 
+  # select just 2019 or 2018 in reference scenario
+  filter(year == 2019 | (year == 2018 & scenario == "Reference"))
+    
+# get change in mean GPD by alcohol use categories
+reference <- output3A %>% filter(scenario == "Reference") %>% ungroup() %>%
+      rename("meangpdref" = "meangpd", "maxref" = "max", "minref" = "min") %>% 
+      dplyr::select(-scenario)
+    
+output3B <- output3A %>% filter(scenario != "Reference") %>% 
+      left_join(., reference) %>% 
+      mutate(diffgpd = meangpd - meangpdref,
+             diffmax = min - minref,
+             diffmin = max - maxref,
+             percgpd = (meangpd - meangpdref) / meangpdref,
+             percmax = (min - minref) / minref,
+             percmin = (max - maxref) / maxref) %>% 
+      dplyr::select(c(scenario, year, sex, education, alc_cat_2018,
+                      diffgpd, diffmin, diffmax,
+                      percgpd, percmin, percmax)) 
+
+# combine elements into Table 3 and formatting
+output3 <- left_join(output3A, output3B) %>% 
+  mutate(mean = case_when(
+          !is.na(meangpd) ~ paste0(format(round(meangpd, digits = 2), nsmall = 2), " (", format(round(min, digits = 2), nsmall = 2), ", ", format(round(max, digits = 2), nsmall = 2), ")"),
+          TRUE ~ NA),
+         diff = case_when(
+           !is.na(diffgpd) ~ paste0(format(round(diffgpd, digits = 2), nsmall = 2), " (", format(round(diffmin, digits = 2), nsmall = 2), ", ", format(round(diffmax, digits = 2), nsmall = 2), ")"),
+           TRUE ~ NA), 
+         perc = case_when(
+           !is.na(percgpd) ~ paste0(format(round(percgpd*100, digits = 2), nsmall = 2), " (", format(round(percmin*100, digits = 2), nsmall = 2), ", ", format(round(percmax*100, digits = 2), nsmall = 2), ")"),
+           TRUE ~ NA)) %>% 
+  dplyr::select(c(setting, alc_cat_2018, scenario, year, sex, education, mean, diff, perc)) %>% 
+  arrange(setting, alc_cat_2018) 
+
+# save all output as XLSX
+write.xlsx(output1, file = paste0(OutputDirectory, Sys.Date(), "/Table_MeanGPD_", Sys.Date(), ".xlsx"), rowNames = F, keepNA = T, na.string = ".")
+write.xlsx(output2, file = paste0(OutputDirectory, Sys.Date(), "/Table_PrevAlcUseCat_", Sys.Date(), ".xlsx"), rowNames = F, keepNA = T, na.string = ".")
+write.xlsx(output3, file = paste0(OutputDirectory, Sys.Date(), "/Table_MeanGPDbyAlcCat_", Sys.Date(), ".xlsx"), rowNames = F, keepNA = T, na.string = ".")
+
