3231c9b56d3edc389c127360740db61490fcbc92
jml-science <80038112+jml-science@users.noreply.github.com>
2025-02-10 11:22:09 +0100

    Compute new CIs
    - Added new CIs by computing max and min difference of mean GPD, prevalence and mean GPD by cat BEFORE averaging over nunc's
    - New CIs are outputted alongside previous version of CIs for direct comparison. Mean effects remain the same

--- pricing_policy/Summary_PricePolicyPaper_Tables.R
+++ pricing_policy/Summary_PricePolicyPaper_Tables.R
@@ -11,8 +11,8 @@ library(tidyr)
 library(data.table)
 library(openxlsx)
 
-WorkingDirectory <- "/Users/carolinkilian/Desktop/SIMAH_workplace/microsim/2_output_data/"
-OutputDirectory <- "/Users/carolinkilian/Desktop/SIMAH_workplace/pricing_policy/"
+WorkingDirectory <- "/Users/julialemp/Desktop/SIMAH_workplace/microsim/2_output_data/"
+OutputDirectory <- "/Users/julialemp/Desktop/SIMAH_workplace/pricing_policy/"
  
 # load data
 data_alccont <- read.csv(paste0(WorkingDirectory, "2025-01-29/output-policy_alcoholcont_2025-01-31.csv"))
@@ -88,9 +88,44 @@ output1B <- output1A %>% filter(scenario != "Reference") %>%
   dplyr::select(c(scenario, setting, year, sex, education,
                   diffgpd, diffmin, diffmax,
                   percgpd, percmin, percmax)) 
+
+# Edit Julia Feb 7, 2025: get mean of change with uncertainty
+diff_2019 <- alccont %>% filter(year == "2019") %>%
+  pivot_wider(names_from = "scenario", values_from = "meansimulation") %>%
+  mutate(across(starts_with("Scenario"), ~ . - Reference, .names = "{.col}_diff")) %>% # compute the difference between the reference scenario and each scenario (1-4) in 2019
+  mutate(across(ends_with("_diff"), ~ (. / Reference), .names = "{.col}_percent"))
+
+# summarise across nunc (mean, min, max)
+output1C <- diff_2019 %>%
+  group_by(setting, year, sex, education) %>%
+  summarise(across(ends_with("_diff"), ~ mean(.x, na.rm = TRUE), .names = "{.col}mean_new"),
+            across(ends_with("_diff"), ~ min(.x, na.rm = TRUE), .names = "{.col}min_new"),
+            across(ends_with("_diff"), ~ max(.x, na.rm = TRUE), .names = "{.col}max_new"),
+            across(ends_with("_percent"), ~ mean(.x, na.rm = TRUE), .names = "{.col}mean_new"),
+            across(ends_with("_percent"), ~ min(.x, na.rm = TRUE), .names = "{.col}min_new"),
+            across(ends_with("_percent"), ~ max(.x, na.rm = TRUE), .names = "{.col}max_new")) %>%
+  ungroup()
+
+output1C = output1C %>%
+  pivot_longer(
+    cols = starts_with("Scenario"), 
+    names_to = c("Scenario", ".value"), 
+    names_pattern = "Scenario (\\d+)_diff(.*)") %>%
+  mutate(Scenario = paste0("Scenario ", Scenario))
  
+output1C = output1C %>%
+  rename_with(~ str_replace(.x, "(mean|min|max)_new", "diff\\1_new")) %>%
+  rename_with(~ str_replace(.x, "_percentdiff(mean|min|max)_new", "perc\\1_new")) %>%
+rename(diffgpd_new = diffmean_new,
+       percgpd_new = percmean_new,
+       scenario = Scenario)
+  
+
 # combine elements into Table 1 and formatting
-output1 <- left_join(output1A, output1B) %>% 
+output1 <- left_join(output1A, output1B) 
+output1 <- left_join(output1, output1C) 
+
+output1 = output1 %>% 
   mutate(mean = case_when(
           !is.na(meangpd) & year != 2000 ~ paste0(format(round(meangpd, digits = 2), nsmall = 2), " (", format(round(min, digits = 2), nsmall = 2), ", ", format(round(max, digits = 2), nsmall = 2), ")"),
           !is.na(meangpd) & year == 2000 ~ paste0(format(round(meangpd, digits = 2), nsmall = 2)),
@@ -99,11 +134,19 @@ output1 <- left_join(output1A, output1B) %>%
            !is.na(diffgpd) & year != 2000 ~ paste0(format(round(diffgpd, digits = 2), nsmall = 2), " (", format(round(diffmax, digits = 2), nsmall = 2), ", ", format(round(diffmin, digits = 2), nsmall = 2), ")"),
            !is.na(diffgpd) & year == 2000 ~ paste0(format(round(diffgpd, digits = 2), nsmall = 2)),
            TRUE ~ NA),
+         diff_new = case_when(
+           !is.na(diffgpd_new) & year != 2000 ~ paste0(format(round(diffgpd_new, digits = 2), nsmall = 2), " (", format(round(diffmin_new, digits = 2), nsmall = 2), ", ", format(round(diffmax_new, digits = 2), nsmall = 2), ")"),
+           !is.na(diffgpd_new) & year == 2000 ~ paste0(format(round(diffgpd_new, digits = 2), nsmall = 2)),
+           TRUE ~ NA),
          perc = case_when(
            !is.na(percgpd) & year != 2000 ~ paste0(format(round(percgpd*100, digits = 2), nsmall = 2), " (", format(round(percmax*100, digits = 2), nsmall = 2), ", ", format(round(percmin*100, digits = 2), nsmall = 2), ")"),
            !is.na(percgpd) & year == 2000 ~ paste0(format(round(percgpd*100, digits = 2), nsmall = 2)),
+           TRUE ~ NA),
+         perc_new = case_when(
+           !is.na(percgpd_new) & year != 2000 ~ paste0(format(round(percgpd_new*100, digits = 2), nsmall = 2), " (", format(round(percmin_new*100, digits = 2), nsmall = 2), ", ", format(round(percmax_new*100, digits = 2), nsmall = 2), ")"),
+           !is.na(percgpd_new) & year == 2000 ~ paste0(format(round(percgpd_new*100, digits = 2), nsmall = 2)),
            TRUE ~ NA)) %>% 
-  dplyr::select(c(setting, scenario, year, sex, education, mean, diff, perc)) %>% 
+  dplyr::select(c(setting, scenario, year, sex, education, mean, diff, diff_new, perc, perc_new)) %>% 
   arrange(setting)
   
 
@@ -169,8 +212,36 @@ output2B <- output2A %>% filter(scenario != "Reference") %>%
   dplyr::select(c(scenario, setting, year, sex, education, alc_cat,
                   diffprop, diffmin, diffmax)) 
 
+# Edit Julia Feb 7, 2025: get mean of change with uncertainty
+diff_2019 <- alccat %>% filter(year == "2019") %>%
+  pivot_wider(names_from = "scenario", values_from = "propsimulation") %>%
+  mutate(across(starts_with("Scenario"), ~ . - Reference, .names = "{.col}_diff")) # compute the difference between the reference scenario and each scenario (1-4) in 2019
+
+# summarise across nunc (mean, min, max)
+output2C <- diff_2019 %>%
+  group_by(setting, year, sex, education, alc_cat) %>%
+  summarise(across(ends_with("_diff"), ~ mean(.x, na.rm = TRUE), .names = "{.col}prop_new"),
+            across(ends_with("_diff"), ~ min(.x, na.rm = TRUE), .names = "{.col}min_new"),
+            across(ends_with("_diff"), ~ max(.x, na.rm = TRUE), .names = "{.col}max_new")) %>%
+  ungroup()
+
+output2C = output2C %>%
+  pivot_longer(
+    cols = starts_with("Scenario"), 
+    names_to = c("Scenario", ".value"), 
+    names_pattern = "Scenario (\\d+)_diff(.*)") %>%
+  mutate(Scenario = paste0("Scenario ", Scenario))
+
+output2C = output2C %>%
+  rename_with(~ str_replace(.x, "(mean|min|max)_new", "diff\\1_new")) %>%
+  rename(diffprop_new = prop_new,
+         scenario = Scenario)
+
 # combine elements into Table 2 and formatting
-output2 <- left_join(output2A, output2B) %>% 
+output2 <- left_join(output2A, output2B) 
+output2 <- left_join(output2, output2C) 
+
+output2 <- output2 %>% 
   mutate(mean = case_when(
           !is.na(meanprop) & year != 2000 ~ paste0(format(round(meanprop*100, digits = 2), nsmall = 2), " (", format(round(min*100, digits = 2), nsmall = 2), ", ", format(round(max*100, digits = 2), nsmall = 2), ")"),
           !is.na(meanprop) & year == 2000 ~ paste0(format(round(meanprop*100, digits = 2), nsmall = 2)),
@@ -178,14 +249,18 @@ output2 <- left_join(output2A, output2B) %>%
          diff = case_when(
            !is.na(diffprop) & year != 2000 ~ paste0(format(round(diffprop*100, digits = 2), nsmall = 2), " (", format(round(diffmax*100, digits = 2), nsmall = 2), ", ", format(round(diffmin*100, digits = 2), nsmall = 2), ")"),
            !is.na(diffprop) & year == 2000 ~ paste0(format(round(diffprop*100, digits = 2), nsmall = 2)),
+           TRUE ~ NA),
+         diff_new = case_when(
+           !is.na(diffprop_new) & year != 2000 ~ paste0(format(round(diffprop_new*100, digits = 2), nsmall = 2), " (", format(round(diffmin_new*100, digits = 2), nsmall = 2), ", ", format(round(diffmax_new*100, digits = 2), nsmall = 2), ")"),
+           !is.na(diffprop_new) & year == 2000 ~ paste0(format(round(diffprop_new*100, digits = 2), nsmall = 2)),
            TRUE ~ NA)) %>% 
-  dplyr::select(c(setting, scenario, year, sex, education, mean, diff, alc_cat)) %>% 
-  pivot_wider(names_from = alc_cat, values_from = c(mean, diff)) %>% 
+  dplyr::select(c(setting, scenario, year, sex, education, mean, diff, diff_new, alc_cat)) %>% 
+  pivot_wider(names_from = alc_cat, values_from = c(mean, diff, diff_new)) %>% 
   dplyr::select(c(setting, scenario, year, sex, education, 
-                  mean_Abstinence, diff_Abstinence,
-                  `mean_Category I`, `diff_Category I`,
-                  `mean_Category II`, `diff_Category II`,
-                  `mean_Category III`, `diff_Category III`)) %>% 
+                  mean_Abstinence, diff_Abstinence, diff_new_Abstinence,
+                  `mean_Category I`, `diff_Category I`, `diff_new_Category I`,
+                  `mean_Category II`, `diff_Category II`, `diff_new_Category II`,
+                  `mean_Category III`, `diff_Category III`, `diff_new_Category III`)) %>% 
   arrange(setting)
 
 
@@ -257,18 +332,60 @@ output3B <- output3A %>% filter(scenario != "Reference") %>%
                       diffgpd, diffmin, diffmax,
                       percgpd, percmin, percmax)) 
 
+
+# Edit Julia Feb 7, 2025: get mean of change with uncertainty
+diff_2019 <- alccontcat %>% filter(year == "2019") %>%
+  dplyr::select(-sesimulation) %>%
+  tidyr::pivot_wider(names_from = "scenario", values_from = "meansimulation") %>%
+  mutate(across(starts_with("Scenario"), ~ . - Reference, .names = "{.col}_diff")) %>% # compute the difference between the reference scenario and each scenario (1-4) in 2019
+  mutate(across(ends_with("_diff"), ~ (. / Reference), .names = "{.col}_percent"))
+
+# summarise across nunc (mean, min, max)
+output3C <- diff_2019 %>%
+  group_by(setting, year, sex, education, alc_cat_2018) %>%
+  summarise(across(ends_with("_diff"), ~ mean(.x, na.rm = TRUE), .names = "{.col}mean_new"),
+            across(ends_with("_diff"), ~ min(.x, na.rm = TRUE), .names = "{.col}min_new"),
+            across(ends_with("_diff"), ~ max(.x, na.rm = TRUE), .names = "{.col}max_new"),
+            across(ends_with("_percent"), ~ mean(.x, na.rm = TRUE), .names = "{.col}mean_new"),
+            across(ends_with("_percent"), ~ min(.x, na.rm = TRUE), .names = "{.col}min_new"),
+            across(ends_with("_percent"), ~ max(.x, na.rm = TRUE), .names = "{.col}max_new")) %>%
+  ungroup()
+
+output3C = output3C %>%
+  pivot_longer(
+    cols = starts_with("Scenario"), 
+    names_to = c("Scenario", ".value"), 
+    names_pattern = "Scenario (\\d+)_diff(.*)") %>%
+  mutate(Scenario = paste0("Scenario ", Scenario))
+
+output3C = output3C %>%
+  rename_with(~ str_replace(.x, "(mean|min|max)_new", "diff\\1_new")) %>%
+  rename_with(~ str_replace(.x, "_percentdiff(mean|min|max)_new", "perc\\1_new")) %>%
+  rename(diffgpd_new = diffmean_new,
+         percgpd_new = percmean_new,
+         scenario = Scenario)
+
+
+
 # combine elements into Table 3 and formatting
-output3 <- left_join(output3A, output3B) %>% 
+output3 <- left_join(output3A, output3B) 
+output3 <- left_join(output3, output3C) %>% 
   mutate(mean = case_when(
           !is.na(meangpd) ~ paste0(format(round(meangpd, digits = 2), nsmall = 2), " (", format(round(min, digits = 2), nsmall = 2), ", ", format(round(max, digits = 2), nsmall = 2), ")"),
           TRUE ~ NA),
          diff = case_when(
            !is.na(diffgpd) ~ paste0(format(round(diffgpd, digits = 2), nsmall = 2), " (", format(round(diffmax, digits = 2), nsmall = 2), ", ", format(round(diffmin, digits = 2), nsmall = 2), ")"),
            TRUE ~ NA), 
+         diff_new = case_when(
+           !is.na(diffgpd_new) ~ paste0(format(round(diffgpd_new, digits = 2), nsmall = 2), " (", format(round(diffmin_new, digits = 2), nsmall = 2), ", ", format(round(diffmax_new, digits = 2), nsmall = 2), ")"),
+           TRUE ~ NA), 
          perc = case_when(
            !is.na(percgpd) ~ paste0(format(round(percgpd*100, digits = 2), nsmall = 2), " (", format(round(percmax*100, digits = 2), nsmall = 2), ", ", format(round(percmin*100, digits = 2), nsmall = 2), ")"),
+           TRUE ~ NA),
+         perc_new = case_when(
+           !is.na(percgpd_new) ~ paste0(format(round(percgpd_new*100, digits = 2), nsmall = 2), " (", format(round(percmin_new*100, digits = 2), nsmall = 2), ", ", format(round(percmax_new*100, digits = 2), nsmall = 2), ")"),
            TRUE ~ NA)) %>% 
-  dplyr::select(c(setting, alc_cat_2018, scenario, year, sex, education, mean, diff, perc)) %>% 
+  dplyr::select(c(setting, alc_cat_2018, scenario, year, sex, education, mean, diff, diff_new, perc, perc_new)) %>% 
   arrange(setting, alc_cat_2018) 
 
 # save all output as XLSX
