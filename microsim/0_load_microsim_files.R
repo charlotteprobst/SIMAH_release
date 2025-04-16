aa219fa9af7411d8ca6e5c04ce7b32727cbefb2f
CharlotteProbst <12916002+charlotteprobst@users.noreply.github.com>
2025-04-16 10:54:01 -0400

    Update 0_load_microsim_files.R
    Simplified data paths

--- microsim/0_load_microsim_files.R
+++ microsim/0_load_microsim_files.R
@@ -17,7 +17,7 @@ death_counts <- load_death_counts(proportion, SelectedState, DataDirectory)
 
 # read in migration in and out counts
 # these can be generated in the folder 1_migration_calibration if needed 
-migration_rates <- read.csv(paste0("SIMAH_workplace/microsim/1_input_data/birth_migration_rates_", SelectedState, ".csv"))
+migration_rates <- read.csv(paste0(DataDirectory,"birth_migration_rates_", SelectedState, ".csv"))
 # load in the education transition rates
 # fix educational attainment at baseline - correct by years of age 
 basepop$YEAR <- 2000
@@ -34,13 +34,13 @@ basepop <- code_alcohol_categories(basepop)
 brfss <- code_alcohol_categories(brfss)
 
 # read in calibrated education transitions 
-education_transitionsList <- read_rds(paste0(WorkingDirectory, "SIMAH_workplace/microsim/2_output_data/education_calibration", "/transitionsList-10",".RDS"))
+education_transitionsList <- read_rds("SIMAH_workplace/microsim/2_output_data/education_calibration/transitionsList-10.RDS")
 for(i in 1:length(education_transitionsList)){
   education_transitionsList[[i]]$cat <- gsub("1999-2019+_","",education_transitionsList[[i]]$cat)
 }
 
 # read in calibrated alcohol transitions
-alcohol_transitions <- read_csv(paste0(WorkingDirectory, "SIMAH_workplace/microsim/2_output_data/alcohol_calibration/lhs_regression-4.csv"), show_col_types = FALSE)
+alcohol_transitions <- read_csv("SIMAH_workplace/microsim/2_output_data/alcohol_calibration/lhs_regression-4.csv", show_col_types = FALSE)
 alcohol_transitionsList <- list()
 for(i in 1:max(alcohol_transitions$sample)){
   alcohol_transitionsList[[i]] <- alcohol_transitions %>% filter(sample==i)
